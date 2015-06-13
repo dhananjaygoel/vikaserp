030086abbd6aaec272c2252c396f3492ab50c7e8
Shailesh Kondekar <kshailesh@agstechnologies.com>
2015-06-13 16:45:05 +0530

    modified order table

--- database/migrations/2015_06_05_130823_create_order_table.php
+++ database/migrations/2015_06_05_130823_create_order_table.php
@@ -14,15 +14,16 @@ class CreateOrderTable extends Migration {
         Schema::create('order', function(Blueprint $table) {
             $table->increments('id')->comment('primary key');
             $table->enum('order_source', array('warehouse', 'supplier'))->comment('order fulfilled through');
+            $table->integer('supplier_id')->comment('Supplier associated with the order');
             $table->integer('customer_id')->comment('Customer associated with the order');
             $table->integer('created_by')->comment('Who created this order');
             $table->integer('delivery_location_id')->comment('Order delivery location');
-            $table->int('vat_percent')->comment('Vat percent id applied');
-            $table->integer('estimated_price')->comment('Estimated price of the order');
-            $table->integer('estimated_delivery_date')->comment('Estimated delivery date');
-            $table->string('expected_delivery_date')->comment('Expected delivery date');
+            $table->string('vat_percentage',100)->comment('Vat percent id applied');            
+            $table->date('estimated_delivery_date')->comment('Estimated delivery date');
+            $table->date('expected_delivery_date')->comment('Expected delivery date');
             $table->string('remarks')->comment('Creator comment on the order');
             $table->enum('order_status', array('pending', 'completed', 'cancelled'))->comment('order status');
+            $table->string('other_location')->comment('Contains other delivery location');
             $table->timestamps();
         });
     }
