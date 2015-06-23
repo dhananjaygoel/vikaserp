db98519fef29c74ff8a3964dd27f8bc6e74c74b6
Dilip Chaudhary <kdilip@agstechnologies.com>
2015-06-23 14:28:27 +0530

    mail

--- config/mail.php
+++ config/mail.php
@@ -16,8 +16,8 @@ return [
      */
 
 //	'driver' => env('MAIL_DRIVER', 'smtp'),
-//    'driver' => 'mail',
-    'driver' => 'smtp',
+    'driver' => 'mail',
+//    'driver' => 'smtp',
     /*
       |--------------------------------------------------------------------------
       | SMTP Host Address
