1dcb16660b064e323966ee86c54c809bcb81ed78
Dilip Chaudhary <kdilip@agstechnologies.com>
2015-06-23 15:17:58 +0530

    mail

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -209,9 +209,17 @@ class PurchaseOrderController extends Controller {
      */
     public function update($id) {
 
-        Mail::raw('Laravel with Mailgun is easy!', function($message) {
-            $message->to('kdilip@agstechnologies.com');
+//                        $data = array('from' => 'admin@steel-trading-automation.com', 'to' => $customers->email);
+        $data = array('from' => 'admin@steel-trading-automation.com', 'to' => 'kdilip@agstechnologies.com');
+
+        Mail::send('emails.purchase_order', array('name' => '$customers->owner_name'), function($message) use ($data) {
+            $message->from($data['from']);
+            $message->to($data['to'])->subject('Updated of the order');
         });
+
+//        Mail::raw('Laravel with Mailgun is easy!', function($message) {
+//            $message->to('kdilip@agstechnologies.com');
+//        });
 
         $input_data = Input::all();
 
