bc68e6716f4b35725f7088e5e9105f1c0279f6d2
Shailesh Kondekar <kshailesh@agstechnologies.com>
2015-07-10 11:11:53 +0530

    order

--- database/migrations/2015_06_13_063346_create_all_order_products_table.php
+++ database/migrations/2015_06_13_063346_create_all_order_products_table.php
@@ -22,8 +22,7 @@ class CreateAllOrderProductsTable extends Migration {
                         $table->integer('actual_quantity')->comment('Actual Quantity for product');
                         $table->integer('quantity')->comment('Quantity for product');
                         $table->integer('actual_pieces')->comment('Actual pieces of product');
-                        $table->integer('price')->comment('Price for product');
-                        $table->string('from')->comment('order from which module');
+                        $table->integer('price')->comment('Price for product');      
                         $table->integer('present_shipping')->comment('Present shipping');
                         $table->text('remarks')->comment('Individual Remarks for product');
                         $table->softDeletes();
