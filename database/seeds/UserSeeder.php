e3561ac36bcd36e6c034f4ae9a1c7e34b9a8ede9
Shailesh Kondekar <kshailesh@agstechnologies.com>
2015-07-13 13:08:47 +0530

    users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -9,6 +9,7 @@ class UserSeeder extends Seeder {
         DB::table('users')->truncate();
         DB::table('users')->insert(
                 array(
+                    //Super Admin
                     array(
                         'id' => '1',
                         'first_name' => 'Super',
@@ -19,6 +20,7 @@ class UserSeeder extends Seeder {
                         'password' => '$2y$10$CscOJeTOQHM3cLOBmIRFT.7owVhR7NUjT/UTlo3Z9PR3SS9w3m.Ia',
                         'role_id' => '0'
                     ),
+                    //Admin
                     array(
                         'id' => '2',
                         'first_name' => 'Admin',
@@ -29,25 +31,38 @@ class UserSeeder extends Seeder {
                         'password' => '$2y$10$CscOJeTOQHM3cLOBmIRFT.7owVhR7NUjT/UTlo3Z9PR3SS9w3m.Ia',
                         'role_id' => '1'
                     ),
+                    //Sales Staff
                     array(
                         'id' => '3',
-                        'first_name' => 'Sales',
-                        'last_name' => 'Staff',
+                        'first_name' => 'Ajay Sales',
+                        'last_name' => 'Wagh',
                         'phone_number' => '1234567890',
-                        'mobile_number' => '989898993',
+                        'mobile_number' => '9898989892',
                         'email' => 's1@s1.com',
                         'password' => '$2y$10$CscOJeTOQHM3cLOBmIRFT.7owVhR7NUjT/UTlo3Z9PR3SS9w3m.Ia',
                         'role_id' => '2'
                     ),
+                    // Delivery Staff
                     array(
                         'id' => '4',
-                        'first_name' => 'Delivery',
-                        'last_name' => 'Staff',
+                        'first_name' => 'Vijay Delivery',
+                        'last_name' => 'Dighe',
                         'phone_number' => '1234567890',
-                        'mobile_number' => '989898994',
+                        'mobile_number' => '9898989893',
                         'email' => 'd1@d1.com',
                         'password' => '$2y$10$CscOJeTOQHM3cLOBmIRFT.7owVhR7NUjT/UTlo3Z9PR3SS9w3m.Ia',
                         'role_id' => '3'
+                    ),
+                    //Account User
+                    array(
+                        'id' => '5',
+                        'first_name' => 'Dinesh',
+                        'last_name' => 'Ingale',
+                        'phone_number' => '1234567890',
+                        'mobile_number' => '9898989894',
+                        'email' => 'd1@d1.com',
+                        'password' => '$2y$10$CscOJeTOQHM3cLOBmIRFT.7owVhR7NUjT/UTlo3Z9PR3SS9w3m.Ia',
+                        'role_id' => '4'
                     )
         ));
     }
