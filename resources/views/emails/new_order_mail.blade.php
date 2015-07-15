718d88cf4ced4f3882edd90c024edf70462f4831
Aman Agarwal <amana@agstechnologies.com>
2015-07-15 15:17:01 +0530

    Edit Email template

--- resources/views/emails/new_order_mail.blade.php
+++ resources/views/emails/new_order_mail.blade.php
@@ -1,25 +1,25 @@
 <div class="invoice">
-    <p>Dear {{$order['customer_name']}}, </p>
-    <p>New order has been generated for your enquiry on {{$order['created_date']}}</p>
+    <p>Dear <em>{{$order['customer_name']}}</em>, </p>
+    <p>New order has been generated for your enquiry on <strong>{{$order['created_date']}}</strong></p>
     <p>Here are the details for your order,</p>
-    <div class="divTable">                
-        <div class="headRow">
-            <div  class="divCell">Sr.</div>
-            <div  class="divCell">Product</div>
-            <div  class="divCell">Size</div>
-            <div  class="divCell">Quantity</div>
-            <div  class="divCell">Rate</div>                
-        </div>
-<?php $i = 1; ?>
+    <table border='1'>
+        <tr>
+            <th>Sr no.</th>
+            <th>Product</th>
+            <th>Size</th>
+            <th>Quantity</th>
+            <th>Rate</th>
+        </tr>
+        <?php $i = 1; ?>
         @foreach($order['order_product'] as $product)
-        <div class="divRow">
-            <div class="divCell">{{ $i++ }}</div>
-            <div class="divCell">{{ $product['order_product_details']->alias_name }}</div>
-            <div class="divCell">{{ $product['order_product_details']->size }}</div>
-            <div class="divCell">{{ $product->quantity }}</div>
-            <div class="divCell">{{ $product->price }}</div>
-        </div>
+        <tr>
+            <td>{{$i++}}</td>
+            <td>{{ $product['order_product_details']->alias_name }}</td>
+            <td>{{ $product['order_product_details']->size }}</td>
+            <td>{{ $product->quantity }}</td>
+            <td>{{ $product->price }}</td>
+        </tr>
         @endforeach
-    </div>
-    <p>You order will be delivered By: {{$order['expected_delivery_date']}}</p>
+    </table>
+    <p>You order will be delivered By: <strong>{{$order['expected_delivery_date']}}</strong></p>
 </div>
