b4479672bdef49c0f27b467c9611953a995d59f8
Dilip Chaudhary <kdilip@agstechnologies.com>
2015-07-17 15:10:58 +0530

    script

--- resources/assets/custom_js/custom_script_js.js
+++ resources/assets/custom_js/custom_script_js.js
@@ -236,7 +236,7 @@ function getunit_name(key) {
 function change_quantity(key) {
 
     var quantity = $("#pending_qunatity_value_" + key).val();
-    var present_shipping = $("#present_shipping_" + key).val();//ok
+    var present_shipping = $("#present_shipping_" + key).val();
     var tot_quty = $("#quantity_" + key).val(); //ok
     var total = parseInt(quantity) + parseInt(present_shipping);
 
