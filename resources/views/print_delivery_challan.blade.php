0675d3d75ed6939624eb0bce6219178b3987a6cf
Dilip Chaudhary <kdilip@agstechnologies.com>
2015-07-27 21:10:35 +0530

    delivery challan

--- resources/views/print_delivery_challan.blade.php
+++ resources/views/print_delivery_challan.blade.php
@@ -23,7 +23,7 @@
             .divCell2{
                 float:left;
                 display:table-column;         
-                width:10%;         
+                width:6%;         
                 padding: 5px;
                 border-right: 1px solid #ccc;
             }
@@ -59,19 +59,19 @@
             }
             .total-desc
             {
-                width: 70%;
+                width: 65%;
                 float: left;
 
             }
             .total
             {
-                width:30%;
+                width:35%;
                 float: left;
             }
             .invoice
             {
-                width:90%;
-                margin-left: 5%;
+                width:100%;
+                /*margin-left: 5%;*/
                 border: 1px solid #ccc;
                 float: left;
                 padding: 0px;
@@ -175,9 +175,9 @@
             }
             .value
             {
-                width: 50%;
+                /*width: 50%;*/
                 text-align: right;
-                float: left;
+                float: right;
                 border-top: 1px solid rgb(204, 204, 204);
                 border-left: 1px solid #ccc;
             }
@@ -231,13 +231,16 @@
                 $total_price = 0;
                 $total_qty = 0;
                 ?>
-                @foreach($allorder['all_order_products'] as $prod)
+
+                @foreach($allorder['delivery_challan_products'] as $prod)
+
+
                 @if($prod->order_type == 'delivery_challan')
                 <div class="divRow">
                     <div class="divCell2">{{ $i++ }}</div>
                     <div class="divCell">{{ $prod['order_product_details']->alias_name }}</div>
                     <div class="divCell">{{ $prod->actual_pieces }}</div>
-                    <div class="divCell">{{ $prod->quantity }}</div>
+                    <div class="divCell">{{ $prod->actual_quantity }}</div>
                     <div class="divCell"> 
                         <?php
                         $difference_amount = 0;
@@ -250,26 +253,23 @@
                         ?>
                     </div>
                     <div class="divCell">
-                        <?php $total_price += $rate * $prod->quantity; ?> 
+                        <?php $total_price += $rate * $prod->actual_quantity; ?> 
                         {{ $total_price }} 
                     </div>                
                 </div>
-
                 <?php
-                $total_qty = 0;
-                foreach ($allorder['delivery_challan_products'] as $del_product) {
+                if ($prod->unit_id == 1) {
+                    $total_qty += $prod->actual_quantity;
+                }
 
-                    if ($prod['unit']->unit_name == 'KG') {
-                        $total_qty += $del_product->quantity;
-                    }
+                if ($prod->unit_id == 2) {
+                    echo $prod['order_product_details']['product_category']->weight;
+                    $total_qty += $prod->actual_quantity * $prod['order_product_details']->weight;
+                }
 
-                    if ($prod['unit']->unit_name == 'Pieces') {
-                        $total_qty += $del_product->quantity * $prod['order_product_details']->weight;
-                    }
-
-                    if ($prod['unit']->unit_name == 'Meter') {
-                        $total_qty += ($del_product->quantity / $prod['order_product_details']->standard_length) * $prod['order_product_details']->weight;
-                    }
+                if ($prod->unit_id == 3) {
+                    echo $prod['order_product_details']['product_category']->standard_length;
+                    $total_qty += ($prod->actual_quantity / $prod['order_product_details']->standard_length) * $prod['order_product_details']->weight;
                 }
                 ?>
                 @endif
@@ -279,16 +279,16 @@
             <div class="footer">
                 <div class="total-desc">
                     <div class="quantity">
-                        Total Quantity: {{ $total_qty }}
+                        Total Quantity: {{ round($total_qty, 2) }}
                     </div>
                     <div class="ruppes grand_price">
-                        Rs. <?php echo convert_number_to_words($total_price + $allorder->freight + $allorder->loading_charge + $allorder->round_off + $allorder->discount + $allorder->vat_percentage / 100 * 100); ?> Rupees
+                        Rupees. <?php echo convert_number_to_words($total_price + $allorder->freight + $allorder->loading_charge + $allorder->discount + $allorder->round_off + $allorder->vat_percentage / 100 * 100); ?> Only
                     </div>
                 </div>
                 <div class="total">                 
                     <div class="">
                         <div class="label">Total</div>
-                        <div class="value bob"> {{ $total_price }} </div>
+                        <div class="value bob"> {{ round($total_price, 2) }} </div>
                         <div class="label ">Loading</div>
                         <div class="value"> 
                             @if($allorder->loading_charge != "")
@@ -313,10 +313,11 @@
                             0
                             @endif 
                         </div>                
-                        <!--                        <div class="label">Total</div>
-                                                <div class="value">
-                                                   {{ $total_price + $allorder->loaded_by + $allorder->freight - $allorder->discount }}
-                                                </div>-->
+                        <div class="label">Total</div>
+                        <div class="value">
+                            <?php $with_total = $total_price + $allorder->loading_charge + $allorder->freight + $allorder->discount; ?>
+                            {{ round($with_total, 2) }}
+                        </div>
                         <div class="label">Vat</div>
                         <div class="value">
                             @if($allorder->vat_percentage != "")
@@ -335,125 +336,124 @@
                         </div>
                         <div class="label">GT</div>
                         <div class="value">
-                            {{ $total_price + $allorder->freight + $allorder->loading_charge + $allorder->round_off + $allorder->discount + $allorder->vat_percentage/100 * 100 }}
+<?php $gt = $total_price + $allorder->freight + $allorder->loading_charge + $allorder->round_off + $allorder->discount + $allorder->vat_percentage / 100 * 100; ?>
+                            {{ round($gt, 2) }}
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-        <?php
+<?php
 
-        function convert_number_to_words($number) {
+function convert_number_to_words($number) {
+    $hyphen = '-';
+    $conjunction = ' and ';
+    $separator = ', ';
+    $negative = 'negative ';
+    $decimal = ' point ';
+    $dictionary = array(
+        0 => 'zero',
+        1 => 'one',
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
+        10 => 'ten',
+        11 => 'eleven',
+        12 => 'twelve',
+        13 => 'thirteen',
+        14 => 'fourteen',
+        15 => 'fifteen',
+        16 => 'sixteen',
+        17 => 'seventeen',
+        18 => 'eighteen',
+        19 => 'nineteen',
+        20 => 'twenty',
+        30 => 'thirty',
+        40 => 'fourty',
+        50 => 'fifty',
+        60 => 'sixty',
+        70 => 'seventy',
+        80 => 'eighty',
+        90 => 'ninety',
+        100 => 'hundred',
+        1000 => 'thousand',
+        1000000 => 'million',
+        1000000000 => 'billion',
+        1000000000000 => 'trillion',
+        1000000000000000 => 'quadrillion',
+        1000000000000000000 => 'quintillion'
+    );
 
-            $hyphen = '-';
-            $conjunction = ' and ';
-            $separator = ', ';
-            $negative = 'negative ';
-            $decimal = ' point ';
-            $dictionary = array(
-                0 => 'zero',
-                1 => 'one',
-                2 => 'two',
-                3 => 'three',
-                4 => 'four',
-                5 => 'five',
-                6 => 'six',
-                7 => 'seven',
-                8 => 'eight',
-                9 => 'nine',
-                10 => 'ten',
-                11 => 'eleven',
-                12 => 'twelve',
-                13 => 'thirteen',
-                14 => 'fourteen',
-                15 => 'fifteen',
-                16 => 'sixteen',
-                17 => 'seventeen',
-                18 => 'eighteen',
-                19 => 'nineteen',
-                20 => 'twenty',
-                30 => 'thirty',
-                40 => 'fourty',
-                50 => 'fifty',
-                60 => 'sixty',
-                70 => 'seventy',
-                80 => 'eighty',
-                90 => 'ninety',
-                100 => 'hundred',
-                1000 => 'thousand',
-                1000000 => 'million',
-                1000000000 => 'billion',
-                1000000000000 => 'trillion',
-                1000000000000000 => 'quadrillion',
-                1000000000000000000 => 'quintillion'
-            );
+    if (!is_numeric($number)) {
+        return false;
+    }
 
-            if (!is_numeric($number)) {
-                return false;
+    if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
+        // overflow
+        trigger_error(
+                'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX, E_USER_WARNING
+        );
+        return false;
+    }
+
+    if ($number < 0) {
+        return $negative . convert_number_to_words(abs($number));
+    }
+
+    $string = $fraction = null;
+
+    if (strpos($number, '.') !== false) {
+        list($number, $fraction) = explode('.', $number);
+    }
+
+    switch (true) {
+        case $number < 21:
+            $string = $dictionary[$number];
+            break;
+        case $number < 100:
+            $tens = ((int) ($number / 10)) * 10;
+            $units = $number % 10;
+            $string = $dictionary[$tens];
+            if ($units) {
+                $string .= $hyphen . $dictionary[$units];
             }
-
-            if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
-                // overflow
-                trigger_error(
-                        'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX, E_USER_WARNING
-                );
-                return false;
+            break;
+        case $number < 1000:
+            $hundreds = $number / 100;
+            $remainder = $number % 100;
+            $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+            if ($remainder) {
+                $string .= $conjunction . convert_number_to_words($remainder);
             }
-
-            if ($number < 0) {
-                return $negative . convert_number_to_words(abs($number));
+            break;
+        default:
+            $baseUnit = pow(1000, floor(log($number, 1000)));
+            $numBaseUnits = (int) ($number / $baseUnit);
+            $remainder = $number % $baseUnit;
+            $string = convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+            if ($remainder) {
+                $string .= $remainder < 100 ? $conjunction : $separator;
+                $string .= convert_number_to_words($remainder);
             }
+            break;
+    }
 
-            $string = $fraction = null;
-
-            if (strpos($number, '.') !== false) {
-                list($number, $fraction) = explode('.', $number);
-            }
-
-            switch (true) {
-                case $number < 21:
-                    $string = $dictionary[$number];
-                    break;
-                case $number < 100:
-                    $tens = ((int) ($number / 10)) * 10;
-                    $units = $number % 10;
-                    $string = $dictionary[$tens];
-                    if ($units) {
-                        $string .= $hyphen . $dictionary[$units];
-                    }
-                    break;
-                case $number < 1000:
-                    $hundreds = $number / 100;
-                    $remainder = $number % 100;
-                    $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
-                    if ($remainder) {
-                        $string .= $conjunction . convert_number_to_words($remainder);
-                    }
-                    break;
-                default:
-                    $baseUnit = pow(1000, floor(log($number, 1000)));
-                    $numBaseUnits = (int) ($number / $baseUnit);
-                    $remainder = $number % $baseUnit;
-                    $string = convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
-                    if ($remainder) {
-                        $string .= $remainder < 100 ? $conjunction : $separator;
-                        $string .= convert_number_to_words($remainder);
-                    }
-                    break;
-            }
-
-            if (null !== $fraction && is_numeric($fraction)) {
-                $string .= $decimal;
-                $words = array();
-                foreach (str_split((string) $fraction) as $number) {
-                    $words[] = $dictionary[$number];
-                }
-                $string .= implode(' ', $words);
-            }
-
-            return $string;
+    if (null !== $fraction && is_numeric($fraction)) {
+        $string .= $decimal;
+        $words = array();
+        foreach (str_split((string) $fraction) as $number) {
+            $words[] = $dictionary[$number];
         }
-        ?>
+        $string .= implode(' ', $words);
+    }
+
+    return $string;
+}
+?>
     </body>
 </html>
