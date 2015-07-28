2334327d67b085655d26a2e280b94512204252a9
Dilip Chaudhary <kdilip@agstechnologies.com>
2015-07-28 14:14:44 +0530

    print delivery challan

--- resources/views/print_delivery_challan.blade.php
+++ resources/views/print_delivery_challan.blade.php
@@ -34,42 +34,33 @@
                 padding: 5px;
                 border-right: 1px solid #ccc;
             }
-            .bob
-            {
+            .bob{
                 border-top: none !important;
             }
-            .divCell:last-child
-            {
+            .divCell:last-child{
                 border: none;
             }
-            .divRow:last-child
-            {
+            .divRow:last-child{
                 border-top: none;
                 border-bottom:  1px solid #ccc;
             }
             .headRow{
                 display:table-row;
             }        
-            .footer
-            {
+            .footer{
                 width: 100%;
                 float: left;
-
-
             }
-            .total-desc
-            {
+            .total-desc{
                 width: 65%;
                 float: left;
 
             }
-            .total
-            {
+            .total{
                 width:35%;
                 float: left;
             }
-            .invoice
-            {
+            .invoice{
                 width:100%;
                 /*margin-left: 5%;*/
                 border: 1px solid #ccc;
@@ -78,67 +69,57 @@
                 overflow: hidden;
             }
 
-            .time
-            {
+            .time{
                 width: 100%;
                 float: left;
                 padding: 10px 0px 10px 0px;
 
             }
 
-            .time-gen
-            {
+            .time-gen{
                 width: 50%;
                 float: left;
             }
-            .time-prnt
-            {
+            .time-prnt{
                 width: 50%;
                 float: left;
             }
 
-            .delivery-details
-            {
+            .delivery-details{
                 width: 100%;
                 padding: 10px 0px 10px 0px;
                 float: left;
                 border-bottom: 1px solid #ccc;
             }
-            .delivery
-            {
+            .delivery{
                 width: 50%;
                 float: left;
                 position: relative;
             }
-            .estmt-no
-            {
+            .estmt-no {
                 width: 50%;
                 float: left;
                 position: relative;
             }
-            .name-date
-            {
+            .name-date{
                 width: 100%;            
                 padding: 10px 0px 10px 0px;
                 float: left;
                 position: relative;
                 border-bottom: 1px solid #ccc;
             }
-            .name
-            {
+            .name{
                 width: 50%;
                 float: left;
                 position: relative;
             }
-            .date
-            {
+            .date{
                 width: 50%;
                 float: left;
                 position: relative;
             }
 
-            .title
-            {
+            .title{
                 width: 100%;
                 text-align: center;
                 border-bottom: 1px solid #ccc;
@@ -153,29 +134,24 @@
                 text-align: right;
                 margin: 0px;
             }
-            .quantity
-            {
+            .quantity{
                 height: 80px;
             }
-            .label
-            {
-                width: 49%;
+            .label{
+                width: 50%;
                 text-align: left;
                 float: left;
                 border-top: 1px solid rgb(204, 204, 204);
                 border-left: 1px solid rgb(204, 204, 204);
             }
-            .label:first-child
-            {
+            .label:first-child{
                 border-top: none;
             }
-            .value:first-child
-            {
+            .value:first-child {
                 border: none;
             }
-            .value
-            {
-                /*width: 50%;*/
+            .value{
+                width: 48%;
                 text-align: right;
                 float: right;
                 border-top: 1px solid rgb(204, 204, 204);
@@ -336,124 +312,127 @@
                         </div>
                         <div class="label">GT</div>
                         <div class="value">
-<?php $gt = $total_price + $allorder->freight + $allorder->loading_charge + $allorder->round_off + $allorder->discount + $allorder->vat_percentage / 100 * 100; ?>
+                            <?php
+                            $gt = $total_price + $allorder->freight + $allorder->loading_charge + $allorder->round_off + $allorder->discount + $allorder->vat_percentage / 100 * 100;
+                            ?>
                             {{ round($gt, 2) }}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-<?php
 
-function convert_number_to_words($number) {
-    $hyphen = '-';
-    $conjunction = ' and ';
-    $separator = ', ';
-    $negative = 'negative ';
-    $decimal = ' point ';
-    $dictionary = array(
-        0 => 'zero',
-        1 => 'one',
-        2 => 'two',
-        3 => 'three',
-        4 => 'four',
-        5 => 'five',
-        6 => 'six',
-        7 => 'seven',
-        8 => 'eight',
-        9 => 'nine',
-        10 => 'ten',
-        11 => 'eleven',
-        12 => 'twelve',
-        13 => 'thirteen',
-        14 => 'fourteen',
-        15 => 'fifteen',
-        16 => 'sixteen',
-        17 => 'seventeen',
-        18 => 'eighteen',
-        19 => 'nineteen',
-        20 => 'twenty',
-        30 => 'thirty',
-        40 => 'fourty',
-        50 => 'fifty',
-        60 => 'sixty',
-        70 => 'seventy',
-        80 => 'eighty',
-        90 => 'ninety',
-        100 => 'hundred',
-        1000 => 'thousand',
-        1000000 => 'million',
-        1000000000 => 'billion',
-        1000000000000 => 'trillion',
-        1000000000000000 => 'quadrillion',
-        1000000000000000000 => 'quintillion'
-    );
+        <?php
 
-    if (!is_numeric($number)) {
-        return false;
-    }
+        function convert_number_to_words($number) {
+            $hyphen = '-';
+            $conjunction = ' and ';
+            $separator = ', ';
+            $negative = 'negative ';
+            $decimal = ' point ';
+            $dictionary = array(
+                0 => 'zero',
+                1 => 'one',
+                2 => 'two',
+                3 => 'three',
+                4 => 'four',
+                5 => 'five',
+                6 => 'six',
+                7 => 'seven',
+                8 => 'eight',
+                9 => 'nine',
+                10 => 'ten',
+                11 => 'eleven',
+                12 => 'twelve',
+                13 => 'thirteen',
+                14 => 'fourteen',
+                15 => 'fifteen',
+                16 => 'sixteen',
+                17 => 'seventeen',
+                18 => 'eighteen',
+                19 => 'nineteen',
+                20 => 'twenty',
+                30 => 'thirty',
+                40 => 'fourty',
+                50 => 'fifty',
+                60 => 'sixty',
+                70 => 'seventy',
+                80 => 'eighty',
+                90 => 'ninety',
+                100 => 'hundred',
+                1000 => 'thousand',
+                1000000 => 'million',
+                1000000000 => 'billion',
+                1000000000000 => 'trillion',
+                1000000000000000 => 'quadrillion',
+                1000000000000000000 => 'quintillion'
+            );
 
-    if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
-        // overflow
-        trigger_error(
-                'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX, E_USER_WARNING
-        );
-        return false;
-    }
-
-    if ($number < 0) {
-        return $negative . convert_number_to_words(abs($number));
-    }
-
-    $string = $fraction = null;
-
-    if (strpos($number, '.') !== false) {
-        list($number, $fraction) = explode('.', $number);
-    }
-
-    switch (true) {
-        case $number < 21:
-            $string = $dictionary[$number];
-            break;
-        case $number < 100:
-            $tens = ((int) ($number / 10)) * 10;
-            $units = $number % 10;
-            $string = $dictionary[$tens];
-            if ($units) {
-                $string .= $hyphen . $dictionary[$units];
+            if (!is_numeric($number)) {
+                return false;
             }
-            break;
-        case $number < 1000:
-            $hundreds = $number / 100;
-            $remainder = $number % 100;
-            $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
-            if ($remainder) {
-                $string .= $conjunction . convert_number_to_words($remainder);
-            }
-            break;
-        default:
-            $baseUnit = pow(1000, floor(log($number, 1000)));
-            $numBaseUnits = (int) ($number / $baseUnit);
-            $remainder = $number % $baseUnit;
-            $string = convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
-            if ($remainder) {
-                $string .= $remainder < 100 ? $conjunction : $separator;
-                $string .= convert_number_to_words($remainder);
-            }
-            break;
-    }
 
-    if (null !== $fraction && is_numeric($fraction)) {
-        $string .= $decimal;
-        $words = array();
-        foreach (str_split((string) $fraction) as $number) {
-            $words[] = $dictionary[$number];
+            if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
+                // overflow
+                trigger_error(
+                        'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX, E_USER_WARNING
+                );
+                return false;
+            }
+
+            if ($number < 0) {
+                return $negative . convert_number_to_words(abs($number));
+            }
+
+            $string = $fraction = null;
+
+            if (strpos($number, '.') !== false) {
+                list($number, $fraction) = explode('.', $number);
+            }
+
+            switch (true) {
+                case $number < 21:
+                    $string = $dictionary[$number];
+                    break;
+                case $number < 100:
+                    $tens = ((int) ($number / 10)) * 10;
+                    $units = $number % 10;
+                    $string = $dictionary[$tens];
+                    if ($units) {
+                        $string .= $hyphen . $dictionary[$units];
+                    }
+                    break;
+                case $number < 1000:
+                    $hundreds = $number / 100;
+                    $remainder = $number % 100;
+                    $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+                    if ($remainder) {
+                        $string .= $conjunction . convert_number_to_words($remainder);
+                    }
+                    break;
+                default:
+                    $baseUnit = pow(1000, floor(log($number, 1000)));
+                    $numBaseUnits = (int) ($number / $baseUnit);
+                    $remainder = $number % $baseUnit;
+                    $string = convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+                    if ($remainder) {
+                        $string .= $remainder < 100 ? $conjunction : $separator;
+                        $string .= convert_number_to_words($remainder);
+                    }
+                    break;
+            }
+
+            if (null !== $fraction && is_numeric($fraction)) {
+                $string .= $decimal;
+                $words = array();
+                foreach (str_split((string) $fraction) as $number) {
+                    $words[] = $dictionary[$number];
+                }
+                $string .= implode(' ', $words);
+            }
+
+            return $string;
         }
-        $string .= implode(' ', $words);
-    }
-
-    return $string;
-}
-?>
+        ?>
     </body>
 </html>
