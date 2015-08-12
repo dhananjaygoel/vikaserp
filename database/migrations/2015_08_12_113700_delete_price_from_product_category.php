29cc7f91b01a34eeb159ec079972332719d2ecfa
Amit Gupta <gamit@agstechnologies.com>
2015-08-12 17:11:20 +0530

    Delet old column and rename new column in product_category
    To prevent old data

---
+++ database/migrations/2015_08_12_113700_delete_price_from_product_category.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeletePriceFromProductCategory extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('product_category', function($t) {
+            $t->dropColumn('price');
+        });
+        Schema::table('product_category', function($t) {
+            $t->renameColumn('price_new', 'price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        //
+    }
+
+}
