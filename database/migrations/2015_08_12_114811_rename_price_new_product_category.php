4c1befb7c247c5c3bfa81095c45b26807ae43b82
Mrunalini Chavan <cmrunalini@agstechnologies.com>
2015-08-12 17:21:49 +0530

    Modification in migration.

--- database/migrations/2015_08_12_114811_rename_price_new_product_category.php
+++ database/migrations/2015_08_12_114811_rename_price_new_product_category.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DeletePriceFromProductCategory extends Migration {
+class RenamePriceNewProductCategory extends Migration {
 
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class DeletePriceFromProductCategory extends Migration {
      */
     public function up() {
         Schema::table('product_category', function($t) {
-            $t->dropColumn('price');
+            $t->renameColumn('price_new', 'price');
         });
     }
 
