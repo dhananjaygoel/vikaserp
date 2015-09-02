d6dccc3c7655bd4d4736ce2db6032513417afad7
Amit Gupta <gamit@agstechnologies.com>
2015-09-02 19:02:39 +0530

    Sms phone number updated for testing purpose

--- config/smsdata.php
+++ config/smsdata.php
@@ -6,7 +6,8 @@ return [
     'sender_id' => 'VikasPipes',
     'url' => 'http://bulksmspune.mobi/sendurlcomma.aspx',
     'send' => true,
-    'send_sms_to' => '9158344455',
+//    'send_sms_to' => '9158344455',
+    'send_sms_to' => '8983886688',
     'emailData' => array(
         'email' => 'deepakw@agstechnologies.com', 'abhimanipal@gmail.com',
         'name' => 'Deepak Wagh'
