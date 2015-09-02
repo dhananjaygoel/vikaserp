22f086ab009d7e8b585e887149abcf39e8f39a94
Amit Gupta <gamit@agstechnologies.com>
2015-09-02 19:36:14 +0530

    Sms number final for today

--- config/smsdata.php
+++ config/smsdata.php
@@ -6,8 +6,9 @@ return [
     'sender_id' => 'VikasPipes',
     'url' => 'http://bulksmspune.mobi/sendurlcomma.aspx',
     'send' => true,
-    'send_sms_to' => '9158344455',
-    'send_sms_to' => '9975755595',
+    'send_sms_to' => '9373312962',
+//    'send_sms_to' => '9158344455',
+//    'send_sms_to' => '9975755595',
 //    'send_sms_to' => '8983886688',
     'emailData' => array(
         'email' => 'deepakw@agstechnologies.com', 'abhimanipal@gmail.com',
