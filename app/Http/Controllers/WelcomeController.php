1d83e35fc2feb1cc4e7a6ca486124fc8d579d4dd
Amit Gupta <gamit@agstechnologies.com>
2015-09-03 20:25:13 +0530

    Showtable updated

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -689,7 +689,7 @@ class WelcomeController extends Controller {
 
     public function showdata($table_name) {
 //        $pdo = DB::table($table_name)->get();
-        $pdo = DB::table($table_name)->orderby('tally_name', 'ASC')->get();
+        $pdo = DB::table($table_name)->get();
         print('<pre>');
         print_r($pdo);
     }
