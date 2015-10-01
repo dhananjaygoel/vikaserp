0a5177a878ff3def8250f36ebf568c153faf6a5b
Amit Gupta <gamit@agstechnologies.com>
2015-10-01 21:30:47 +0530

    Client email set for order and purchase order

--- config/smsdata.php
+++ config/smsdata.php
@@ -12,7 +12,8 @@ return [
 //    'send_sms_to' => '8983886688', //testing 02-09-2015, 29-09-2015
     'send_sms_to' => '9822232962', //Changed on 18-09-2015
     'emailData' => array(
-        'email' => 'deepakw@agstechnologies.com', 'abhimanipal@gmail.com',
+//        'email' => 'deepakw@agstechnologies.com', 'abhimanipal@gmail.com',
+        'email' => 'abhimanipal@gmail.com',
         'name' => 'Deepak Wagh'
     )
 ];
