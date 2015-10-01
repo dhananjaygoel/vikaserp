814264c33a353f82bd5424202e4694cd81382a6a
Amit Gupta <gamit@agstechnologies.com>
2015-10-01 20:03:36 +0530

    Sms number updated

--- config/smsdata.php
+++ config/smsdata.php
@@ -9,8 +9,8 @@ return [
 //    'send_sms_to' => '9373312962', // Changed on 02-09-2015
 //    'send_sms_to' => '9158344455', // Previous upto 02-09-2015
 //    'send_sms_to' => '9975755595', //testing 02-09-2015
-    'send_sms_to' => '8983886688', //testing 02-09-2015, 29-09-2015
-//    'send_sms_to' => '9822232962', //Changed on 18-09-2015
+//    'send_sms_to' => '8983886688', //testing 02-09-2015, 29-09-2015
+    'send_sms_to' => '9822232962', //Changed on 18-09-2015
     'emailData' => array(
         'email' => 'deepakw@agstechnologies.com', 'abhimanipal@gmail.com', 'gamit@agstechnologies.com',
         'name' => 'Deepak Wagh'
