d5df4a2f3ea5bab5104b39a7185301d8f35b1a14
Amit Gupta <gamit@agstechnologies.com>
2015-10-27 12:57:23 +0530

    Updated - Edit order form product is added

--- resources/views/edit_purchase_order.blade.php
+++ resources/views/edit_purchase_order.blade.php
@@ -177,7 +177,7 @@
                                             <td class="col-md-3">
                                                 <div class="form-group searchproduct">
                                                     <input class="form-control" placeholder="Enter Product name " type="text" name="product[{{$key}}][name]" id="add_purchase_product_name_{{$key}}" value="{{$product['purchase_product_details']->alias_name}}" onfocus="product_autocomplete_purchase({{$key}});">
-                                                    <input type="hidden" name="product[{{$key}}][id]" value="{{$product->product_category_id}}">
+                                                    <input type="hidden" name="product[{{$key}}][id]" id="add_product_id_{{$key}}" value="{{$product->product_category_id}}">
                                                     <i class="fa fa-search search-icon"></i>
                                                 </div>
                                             </td>
