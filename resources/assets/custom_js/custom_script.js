091087cdbc98c975978d53ae8f93d154da0983ad
FirozTennali <firozt@agstechnologies.com>
2015-11-23 21:28:06 +0530

    Update For testing

--- resources/assets/custom_js/custom_script.js
+++ resources/assets/custom_js/custom_script.js
@@ -8,7 +8,7 @@ function setCookie(cname,cvalue,exdays){
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
-}
+} 
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
