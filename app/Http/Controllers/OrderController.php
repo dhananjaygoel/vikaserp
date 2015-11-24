2d26e061456db05afe4aa0ee6b2eb09eda57f5a5
FirozTennali <firozt@agstechnologies.com>
2015-11-24 11:20:56 +0530

    Update for sort order by size name

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -104,9 +104,9 @@ class OrderController extends Controller {
         $allorders->setPath('orders');
 
 
-        echo '<pre>';
-        print_r($allorders->toArray());
-        echo '</pre>';
+//        echo '<pre>';
+//        print_r($allorders->toArray());
+//        echo '</pre>';
 //        exit();
 
         return View::make('orders', compact('delivery_location', 'customers', 'allorders', 'users', 'cancelledorders', 'pending_orders', 'product_size'));
