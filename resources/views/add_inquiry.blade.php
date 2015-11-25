b097636d249913c2745eb623b8d779769ddd2ab4
Aniket Galphade <aniketg@agstechnologies.com>
2015-11-25 21:23:44 +0530

    product name autocomplete

--- resources/views/add_inquiry.blade.php
+++ resources/views/add_inquiry.blade.php
@@ -291,6 +291,10 @@ use Illuminate\Support\Facades\Session;
 //             val = $('#existing_customer_name1').val();
 //          });
           
+          {!! $products =  \Cache::rememberForever('product-sub-category', function () {
+                    return \App\ProductSubCategory::with('product_category')->get();
+              }); !!}
+            
           {!! FormAutocomplete::selector('#existing_customer_name1')->source(function(){
             return \App\Customer::where('customer_status', '=', 'permanent')
                 ->orderBy('tally_name', 'ASC')
