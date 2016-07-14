7ace321d51c33fc835b41efbb0fbf37c6549c459
amitgupta <gamit@agstechnologies.com>
2016-07-14 18:22:42 +0530

    Updated - Time zone for inquiry

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -33,6 +33,7 @@ use Illuminate\Support\Facades\Event;
 class InquiryController extends Controller {
 
     public function __construct() {
+        date_default_timezone_set("Asia/Calcutta");
         define('PROFILE_ID', Config::get('smsdata.profile_id'));
         define('PASS', Config::get('smsdata.password'));
         define('SENDER_ID', Config::get('smsdata.sender_id'));
