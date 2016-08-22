c064b7ce994bb0600146721852ee164e39f8a3e0
Amit Gupta <gamit@agstechnologies.com>
2016-08-22 13:04:02 +0100

    Updated - PO code

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -757,9 +757,9 @@ class PurchaseOrderController extends Controller {
                         }
                     }
                 }
-                $purchase_orders[$key]['pending_quantity'] = ($purchase_order_advise_quantity >= $purchase_order_quantity) ? 0 : ($purchase_order_quantity - $purchase_order_advise_quantity);
-                $purchase_orders[$key]['total_quantity'] = $purchase_order_quantity;
             }
+            $purchase_orders[$key]['pending_quantity'] = ($purchase_order_advise_quantity >= $purchase_order_quantity) ? 0 : ($purchase_order_quantity - $purchase_order_advise_quantity);
+            $purchase_orders[$key]['total_quantity'] = $purchase_order_quantity;
         }
         return $purchase_orders;
     }
