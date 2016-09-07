7ab2c5e716158006ac796be7c7d150302d8ba588
ganeshchhatwani <ganeshc@agstechnologies.com>
2016-09-07 17:55:11 +0530

    Updated for delivery  order

--- resources/views/view_delivery_order.blade.php
+++ resources/views/view_delivery_order.blade.php
@@ -12,10 +12,13 @@
                 <div class="filter-block">
                     <h1 class="pull-left">View Delivery Order</h1>
                     <div class="pull-right top-page-ui">
+                        
+                        @if($delivery_data->order_status == 'pending')
                         @if(($delivery_data->serial_no == "" || Auth::user()->role_id == 0  || Auth::user()->role_id == 1))
                         <a href="{{URL::action('DeliveryOrderController@edit',['id'=>$delivery_data->id])}}" class="btn btn-primary pull-right">
                             Edit Delivery Order
                         </a>
+                        @endif
                         @endif
                     </div>
                 </div>
