3506736031e14ce0f4e24b03a6f926650361b0c6
ganeshchhatwani <ganeshc@agstechnologies.com>
2016-09-07 15:15:10 +0530

    Updated for redirection in change password to dashboard

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -73,7 +73,7 @@
                             <hr>
                             <div >
                                 <button type="submit" class="btn btn-primary form_button_footer" >Submit</button>
-                                <a href="{{URL::previous()}}" class="btn btn-default form_button_footer">Back</a>
+                                <a href="{{URL::to('/dashboard')}}" class="btn btn-default form_button_footer">Back</a>
                             </div>
 
                             <div class="clearfix"></div>
