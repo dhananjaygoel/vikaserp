5037c0dca17028277c36112c9139af0237bb5f8a
atulahire <atula@agstech.co.in>
2016-09-13 18:30:14 +0530

    Fixed - display tally name in inquiry list

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -76,7 +76,7 @@
                                     <tr id="inquiry_row_{{$inquiry['id']}}">
                                         <td class="text-center">{{$i++}}</td>
                                         <td class="text-center">
-                                            {{(isset($inquiry["customer"]->tally_name) && $inquiry["customer"]->tally_name != "")? $inquiry["customer"]->tally_name :isset($inquiry["customer"]->owner_name) ? $inquiry["customer"]->owner_name:''}}
+                                            {{(isset($inquiry["customer"]->tally_name) && $inquiry["customer"]->tally_name != "")? $inquiry["customer"]->tally_name :(isset($inquiry["customer"]->owner_name) ? $inquiry["customer"]->owner_name:'')}}
                                         </td>
 
                                         <?php $qty = 0; ?>
