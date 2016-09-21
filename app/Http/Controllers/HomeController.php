77b3148d02a554b994da79e94ca5e7bf8db39744
atulahire <atula@agstech.co.in>
2016-09-21 14:09:01 +0530

    Test - API -track orders

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -286,8 +286,8 @@ class HomeController extends Controller {
         $order_status_responase=array();
         if(isset($order_id) && $order_id> 0 && isset($customer_id) && $customer_id >0){
             $order_status_responase['order_details'] = Order::where('id','=',$order_id)->where('customer_id','=',$customer_id)->get();
-            $order_status_responase['delivery_order_details'] = DeliveryOrder::with('all_order_products')->where('order_id','=',$order_id)->where('customer_id','=',$customer_id)->get();
-            $order_status_responase['delivery_challan_details'] = DeliveryChallan::with('all_order_products')->where('order_id','=',$order_id)->where('customer_id','=',$customer_id)->get();
+            $order_status_responase['delivery_order_details'] = DeliveryOrder::where('order_id','=',$order_id)->where('customer_id','=',$customer_id)->get();
+            $order_status_responase['delivery_challan_details'] = DeliveryChallan::where('order_id','=',$order_id)->where('customer_id','=',$customer_id)->get();
         }
         else{
             return json_encode(array('result' => false, 'track_order_status' => false, 'message' => 'Order not found'));
