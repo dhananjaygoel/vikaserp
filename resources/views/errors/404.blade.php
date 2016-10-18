220e1a9cdab568f6159363dbaba9f221953d30ad
atulahire <atula@agstech.co.in>
2016-10-18 13:26:59 +0530

    Fixed - typo dashboard

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-12 error_page_div">
         <span class="erro_page_heading">
             We can't find the page you are looking for. Back to
-            <a class="error_page_link" href="{{url('dashboard')}}">Dahsboard</a>
+            <a class="error_page_link" href="{{url('dashboard')}}">Dashboard</a>
         </span>
     </div>
 </div>
