a45765968902e8e808aaac8e551683774d78c5bd
atulahire <atula@agstech.co.in>
2017-01-17 16:23:10 +0530

    update -

--- resources/views/delivery_order.blade.php
+++ resources/views/delivery_order.blade.php
@@ -172,7 +172,7 @@
                                             {{ round($delivery->present_shipping, 2) }}
                                         </td>
                                         <td>
-                                            {{ round($delivery->present_shipping, 2) }}
+                                            {{ round($delivery->pending_order, 2) }}
                                         </td>
                                         <td>
                                             {{$delivery->vehicle_number}}
