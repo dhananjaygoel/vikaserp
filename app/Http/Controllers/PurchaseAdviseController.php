e31832a78bf77e6b7458c5669ee349ac163e643e
atulahire <atula@agstech.co.in>
2017-02-01 15:49:26 +0530

    update : PA create/edit : sms fun

--- app/Http/Controllers/PurchaseAdviseController.php
+++ app/Http/Controllers/PurchaseAdviseController.php
@@ -452,6 +452,48 @@ class PurchaseAdviseController extends Controller {
         $purchase_advice_prod = PurchaseProducts::where('order_type', '=', 'purchase_advice')->where('purchase_order_id', '=', $id)->first();
         $purchase_advise->updated_at = $purchase_advice_prod->updated_at;
         $purchase_advise->save();
+        
+        
+        
+                /*
+         * ------------------- ------------------------
+         * SEND SMS TO Customer FOR edit PURCHASE ADVISE
+         * --------------------------------------------
+         */
+        
+        $purchase_advise = PurchaseAdvise::with('supplier', 'purchase_products.purchase_product_details', 'purchase_products.unit', 'location')->find($id);
+        $input_data = $purchase_advise['purchase_products'];
+        
+      
+            $customer_id = $purchase_advise->supplier_id;
+            $customer = Customer::with('manager')->find($customer_id);
+            if (count($customer) > 0) {
+                $total_quantity = '';
+                $str = "Dear '" . $customer->owner_name . "'\nDT " . date("j M, Y") . "\nYour purchase Advise has been edited as follows ";
+                foreach ($input_data as $product_data) {
+                    $str .= $product_data['purchase_product_details']->alias_name . ' - ' . $product_data->quantity . ' - ' . $product_data->price . ', ';
+                    $total_quantity = $total_quantity + $product_data->quantity;
+                }
+                $str .= " Trk No. " . $purchase_advise->vehicle_number . ".\nVIKAS ASSOCIATES";
+                if (App::environment('development')) {
+                    $phone_number = Config::get('smsdata.send_sms_to');
+                } else {
+//                    $phone_number = $customer->phone_number1;
+                    $phone_number = $customer['manager']->mobile_number;
+                }
+
+                $msg = urlencode($str);
+                $url = SMS_URL . "?user=" . PROFILE_ID . "&pwd=" . PASS . "&senderid=" . SENDER_ID . "&mobileno=" . $phone_number . "&msgtext=" . $msg . "&smstype=0";
+                if (SEND_SMS === true) {
+                    $ch = curl_init($url);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    $curl_scraped_page = curl_exec($ch);
+                    curl_close($ch);
+                }
+            }
+     
+        
+        
         return redirect('purchaseorder_advise')->with('success', 'Purchase advise updated successfully');
     }
 
