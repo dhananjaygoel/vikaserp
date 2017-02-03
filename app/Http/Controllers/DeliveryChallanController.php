773914913638e5ae9a637080603335525f6154bf
atulahire <atula@agstech.co.in>
2017-02-03 18:36:49 +0530

    update : DC Comp :sms text admin

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -641,7 +641,7 @@ class DeliveryChallanController extends Controller {
 //                    $str .= $product->alias_name . ' - ' . $product_data->quantity . ' - ' . $product_data->price . ', ';
                     $total_quantity = $total_quantity + $product_data->quantity;
                 }
-                $str .= " Vehicle No. " . $allorder['delivery_order']->vehicle_number .
+                $s = $str .= " Vehicle No. " . $allorder['delivery_order']->vehicle_number .
                         ", Drv No. " . $allorder['delivery_order']->driver_contact_no .
                         ", Quantity " . $allorder['delivery_challan_products']->sum('actual_quantity') .
                         ", Amount " . $allorder->grand_price .
@@ -664,25 +664,15 @@ class DeliveryChallanController extends Controller {
             }
             if (count($customer['manager']) > 0) {
                 $total_quantity = '';
-                $str1 = "Dear " . $customer['manager']->first_name . "\nDT " . date("j M, Y") . "\n" . Auth::user()->first_name . "  has dispatched material for  " . $customer->owner_name . " as follows\n";
-                foreach ($input_data as $product_data) {
-                    $product = ProductSubCategory::find($product_data->product_category_id);
-//                    $str .= $product->alias_name . ' - ' . $product_data->quantity . ' - ' . $product_data->price . ', ';
-                    $total_quantity = $total_quantity + $product_data->quantity;
-                }
-                $str1 .= " Vehicle No. " . $allorder['delivery_order']->vehicle_number .
-                        ", Drv No. " . $allorder['delivery_order']->driver_contact_no .
-                        ", Quantity " . $allorder['delivery_challan_products']->sum('actual_quantity') .
-                        ", Amount " . $allorder->grand_price .
-                        ", Due by: " . date("j F, Y", strtotime($allorder['delivery_order']->expected_delivery_date)) .
-                        "\nVIKAS ASSOCIATES";
+                $str = "Dear " . $customer['manager']->first_name . "\nDT " . date("j M, Y") . "\n" . Auth::user()->first_name . "  has dispatched material for  " . $customer->owner_name . " as follows\n ". $s;
+               
 
                 if (App::environment('development')) {
                     $phone_number = Config::get('smsdata.send_sms_to');
                 } else {
                     $phone_number = $customer['manager']->mobile_number;
                 }
-                $msg = urlencode($str1);
+                $msg = urlencode($str);
                 $url = SMS_URL . "?user=" . PROFILE_ID . "&pwd=" . PASS . "&senderid=" . SENDER_ID . "&mobileno=" . $phone_number . "&msgtext=" . $msg . "&smstype=0";
                 if (SEND_SMS === true) {
                     $ch = curl_init($url);
