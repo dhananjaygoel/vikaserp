ce7e287f21eea95492dd3121171dfa80ac448492
atulahire <atula@agstech.co.in>
2017-03-07 18:43:00 +0530

    update: DO : D.O. SMS. Price of each item

--- app/Http/Controllers/DeliveryOrderController.php
+++ app/Http/Controllers/DeliveryOrderController.php
@@ -417,16 +417,16 @@ class DeliveryOrderController extends Controller {
             $customer = Customer::with('manager')->find($customer_id);
             if (count($customer) > 0) {
                 $total_quantity = '';
-                $str = "Dear " . $customer->owner_name . "\nDT" . date("j M, Y") . "\nYour DO has been edited as follows ";
+                $str = "Dear " . $customer->owner_name . "\nDT " . date("j M, Y") . "\nYour DO has been edited as follows\n";
                 foreach ($delivery_order['delivery_product'] as $product_data) {
                     $prod = AllOrderProducts::with('product_sub_category')->find($product_data->id);
                     
                     
-                    $str .= $prod['product_sub_category']->alias_name . ' - ' . $prod->quantity . ',';
+                    $str .= $prod['product_sub_category']->alias_name . ' - ' . $prod->quantity . ' - ' . $prod['price'] . ",\n";
                     $total_quantity = $total_quantity + $product_data->quantity;
                 }
                 
-                $str .= " Vehicle No. " . (!empty($delivery_order->vehicle_number)?$delivery_order->vehicle_number:'N/A') . ", Drv No. " . (!empty($delivery_order->driver_contact_no)? $delivery_order->driver_contact_no:'N/A'). ". \nVIKAS ASSOCIATES";
+                $str .= "Vehicle No. " . (!empty($delivery_order->vehicle_number)?$delivery_order->vehicle_number:'N/A') . ", Drv No. " . (!empty($delivery_order->driver_contact_no)? $delivery_order->driver_contact_no:'N/A'). ". \nVIKAS ASSOCIATES";
                 
                
                 if (App::environment('development')) {
@@ -445,16 +445,16 @@ class DeliveryOrderController extends Controller {
             }
             if (count($customer['manager']) > 0) {
                 $total_quantity = '';
-                $str = "Dear " . $customer['manager']->first_name . "\nDT" . date("j M, Y") . "\n".Auth::user()->first_name." has edited DO for " . $customer->owner_name . " as follows ";
+                $str = "Dear " . $customer['manager']->first_name . "\nDT " . date("j M, Y") . "\n".Auth::user()->first_name." has edited DO for " . $customer->owner_name . " as follows\n";
                 foreach ($delivery_order['delivery_product'] as $product_data) {
                     $prod = AllOrderProducts::with('product_sub_category')->find($product_data->id);
                     
                     
-                    $str .= $prod['product_sub_category']->alias_name . ' - ' . $prod->quantity . ',';
+                    $str .= $prod['product_sub_category']->alias_name . ' - ' . $prod->quantity . ' - ' . $prod['price'] . ",\n";
                     $total_quantity = $total_quantity + $product_data->quantity;
                 }
                 
-                $str .= " Vehicle No. " . (!empty($delivery_order->vehicle_number)?$delivery_order->vehicle_number:'N/A') . ", Drv No. " . (!empty($delivery_order->driver_contact_no)? $delivery_order->driver_contact_no:'N/A'). ". \nVIKAS ASSOCIATES";
+                $str .= "Vehicle No. " . (!empty($delivery_order->vehicle_number)?$delivery_order->vehicle_number:'N/A') . ", Drv No. " . (!empty($delivery_order->driver_contact_no)? $delivery_order->driver_contact_no:'N/A'). ". \nVIKAS ASSOCIATES";
                 
                
                 if (App::environment('development')) {
@@ -853,12 +853,12 @@ class DeliveryOrderController extends Controller {
             $customer = Customer::with('manager')->find($customer_id);
             if (count($customer) > 0) {
                 $total_quantity = '';
-                $str = "Dear " . $customer->owner_name . "\nDT" . date("j M, Y") . "\nYour DO has been created as follows ";
+                $str = "Dear " . $customer->owner_name . "\nDT " . date("j M, Y") . "\nYour DO has been created as follows\n";
                 foreach ($input_data as $product_data) {
-                    $str .= $product_data['order_product_details']->alias_name . ' - ' . $product_data->quantity . ',';
+                    $str .= $product_data['order_product_details']->alias_name . ' - ' . $product_data->quantity . ' - ' . $product_data->price . ",\n";
                     $total_quantity = $total_quantity + $product_data->quantity;
                 }
-                $str .= " Vehicle No. " . $delivery_data->vehicle_number . ", Drv No. " . $delivery_data->driver_contact_no . ". \nVIKAS ASSOCIATES";
+                $str .= "Vehicle No. " . $delivery_data->vehicle_number . ", Drv No. " . $delivery_data->driver_contact_no . ". \nVIKAS ASSOCIATES";
                 if (App::environment('development')) {
                     $phone_number = Config::get('smsdata.send_sms_to');
                 } else {
@@ -875,17 +875,18 @@ class DeliveryOrderController extends Controller {
             }
             if (count($customer['manager']) > 0) {
                 $total_quantity = '';
-                $str = "Dear " . $customer['manager']->first_name . "\nDT" . date("j M, Y") . "\n".Auth::user()->first_name." has created DO for " . $customer->owner_name . " as follows ";
+                $str = "Dear " . $customer['manager']->first_name . "\nDT " . date("j M, Y") . "\n".Auth::user()->first_name." has created DO for " . $customer->owner_name . " as follows\n";
                 foreach ($input_data as $product_data) {
-                    $str .= $product_data['order_product_details']->alias_name . ' - ' . $product_data->quantity . ',';
+                    $str .= $product_data['order_product_details']->alias_name . ' - ' . $product_data->quantity . ' - ' . $product_data->price . ",\n";
                     $total_quantity = $total_quantity + $product_data->quantity;
                 }
-                $str .= " Vehicle No. " . $delivery_data->vehicle_number . ", Drv No. " . $delivery_data->driver_contact_no . ". \nVIKAS ASSOCIATES";
+                $str .= "Vehicle No. " . $delivery_data->vehicle_number . ", Drv No. " . $delivery_data->driver_contact_no . ". \nVIKAS ASSOCIATES";
                 if (App::environment('development')) {
                     $phone_number = Config::get('smsdata.send_sms_to');
                 } else {
                     $phone_number = $customer['manager']->mobile_number;
                 }
+               
                 $msg = urlencode($str);
                 $url = SMS_URL . "?user=" . PROFILE_ID . "&pwd=" . PASS . "&senderid=" . SENDER_ID . "&mobileno=" . $phone_number . "&msgtext=" . $msg . "&smstype=0";
                 if (SEND_SMS === true) {
