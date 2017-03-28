113324c287c9a1095e4619c2b59e687dc64d0dbd
atulahire <atula@agstech.co.in>
2017-03-28 15:50:22 +0530

    update: migrate: index

--- vendor/laravel/framework/src/Illuminate/Database/Connection.php
+++ vendor/laravel/framework/src/Illuminate/Database/Connection.php
@@ -88,7 +88,7 @@ class Connection implements ConnectionInterface {
 	 *
 	 * @var bool
 	 */
-	protected $loggingQueries = false;
+	protected $loggingQueries = true;
 
 	/**
 	 * Indicates if the connection is in a "dry run".
@@ -789,7 +789,11 @@ class Connection implements ConnectionInterface {
 	 */
 	public function getDoctrineSchemaManager()
 	{
-		return $this->getDoctrineDriver()->getSchemaManager($this->getDoctrineConnection());
+//		return $this->getDoctrineDriver()->getSchemaManager($this->getDoctrineConnection());
+                
+                $schema = $this->getDoctrineDriver()->getSchemaManager($this->getDoctrineConnection());
+    $schema->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    return $schema;
 	}
 
 	/**
