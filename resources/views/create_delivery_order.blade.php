62bb6b8838d21ed65355f692af8e2efe48b3dd01
atulahire <atula@agstech.co.in>
2017-04-01 12:42:11 +0530

    update:

--- resources/views/create_delivery_order.blade.php
+++ resources/views/create_delivery_order.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('title','Create Delivery Order')
-@section('content')
+@section('content') 
 <div class="row">
     <div class="col-lg-12">
         <div class="row">
