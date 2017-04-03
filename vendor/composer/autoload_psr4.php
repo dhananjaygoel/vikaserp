93425c8a9256cb919b704cffd99aa17a5f2deb15
atulahire <atula@agstech.co.in>
2017-04-03 13:08:30 +0530

    update: dropbox

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -22,6 +22,7 @@ return array(
     'Stringy\\' => array($vendorDir . '/danielstjules/stringy/src'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'League\\Flysystem\\Dropbox\\' => array($vendorDir . '/league/flysystem-dropbox/src'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
     'Illuminate\\Html\\' => array($vendorDir . '/illuminate/html'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
