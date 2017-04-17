ff0af48fdb97ed04de0fbeb4eff353d0d5074be6
Sharayu Naladkar <sharayun@agstechnologies.com>
2017-04-17 18:48:17 +0530

    update for dropdown

--- app/Http/Controllers/LoadByController.php
+++ app/Http/Controllers/LoadByController.php
@@ -140,11 +140,42 @@ class LoadByController extends Controller {
         }
     }
     
-    public function performance(Request $request){
+    public function performance(Request $request) {
+        $var = 0;
+        $loader_arr = array();
+        $loader_array = array();
+        $loaders_data = array();
         $loaded_by = LoadedBy::all();
-        return view('loaded_by_performance')
-                ->with('performance_index',true);
-        
+        $delivery_order_data = DeliveryChallan::with('challan_loaded_by.dc_delivery_challan.delivery_order.delivery_product')->get();
+        foreach ($delivery_order_data as $delivery_order_info) {
+            $arr = array();
+            $loaders = array();
+            if (isset($delivery_order_info->challan_loaded_by) && count($delivery_order_info->challan_loaded_by) > 0 && !empty($delivery_order_info->challan_loaded_by)) {
+                foreach ($delivery_order_info->challan_loaded_by as $challan_info) {
+                    $deliver_sum = 0;
+                    array_push($loaders, $challan_info->loaded_by_id);
+                    foreach ($challan_info->dc_delivery_challan as $info) {
+                        foreach ($info->delivery_order->delivery_product as $delivery_order_productinfo) {
+                            if ($delivery_order_productinfo->unit_id == 1)
+                                $deliver_sum += $delivery_order_productinfo->quantity;
+                            elseif (($delivery_order_productinfo->unit_id == 2) || ($delivery_order_productinfo->unit_id == 3))
+                                $deliver_sum += DashboardController::checkpending_quantity($delivery_order_productinfo->unit_id, $delivery_order_productinfo->product_category_id, $delivery_order_productinfo->quantity);
+                        }
+                    }
+                    array_push($arr, $deliver_sum);
+                    array_push($loader_array, $loaders);
+
+                    $loader_arr['delivery_id'] = $delivery_order_info['id'];
+                    $loader_arr['delivery_date'] = $delivery_order_info['created_at'];
+                    $loader_arr['loaders'] = $loaders;
+                    $loader_arr['delivery_sum'] = $arr;
+                }
+            }
+            $loaders_data[$var] = $loader_arr;
+            $var++;
+        }
+        return view('loaded_by_performance')->with('loaders_data', $loaders_data)
+                        ->with('loaded_by', $loaded_by)
+                        ->with('performance_index', true);
     }
-        
 }
