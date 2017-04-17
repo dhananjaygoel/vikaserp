e68b93f0294f3c29c2d6ddf5053eee828750608f
atulahire <atula@agstech.co.in>
2017-04-17 19:37:59 +0530

    update: beta: multidropdown for DC -labours and loaded by

---
+++ app/DeliveryChallanLabours.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DeliveryChallanLabours extends Model {
+
+    protected $table = 'delivery_challan_labours';
+
+    //
+    public function dc_labour() {
+        return $this->hasMany('App\Labour', 'id', 'labours_id');
+//        return $this->belongsTo('App\LoadedBy', 'id', 'loaded_by_id');
+    }
+
+    public function dc_delivery_challan() {
+        return $this->hasMany('App\DeliveryChallan', 'id', 'delivery_challan_id');
+//        return $this->belongsTo('App\DeliveryChallan', 'id', 'delivery_challan_id');
+    }
+
+}
