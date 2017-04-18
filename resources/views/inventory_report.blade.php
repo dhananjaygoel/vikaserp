6e9163f710be83c3ae906e759d27b784f2ad7f41
Sujeet Koli <ksujeet@agstechnologies.com>
2017-04-18 17:21:46 +0530

    updated for inventory report

--- resources/views/inventory_report.blade.php
+++ resources/views/inventory_report.blade.php
@@ -64,34 +64,26 @@
                                     <tbody>
                                         <tr style="width:50px; height:50px;">
                                             <td class="crossout" colspan="1" rowspan="1"><span class="size-head">Size</span><span class="thickness-head">Thickness</span></td> 
-                                            @if(isset($product_last))
-                                                <?php //  dd($product_last[0]); ?>
-                                                @foreach($product_last[0]['product_sub_categories'] as $sub_cat)
-                                                   <td>{{$sub_cat->thickness}}</td>
+                                            @if(isset($thickness_array))
+                                                @foreach($thickness_array as $thickness)
+                                                   <td>{{$thickness}}</td>
                                                 @endforeach
                                             @endif                                            
-                                        </tr>
-                                        
-                                        @foreach($product_last[0]['product_sub_categories'] as $sub_cat)
-                                        <tr>
-                                            <?php // echo $sub_cat; ?>
-                                            <td>{{$sub_cat->size}}</td>
-                                            <td>
-                                            <?php // print_r($sub_cat['product_inventory']); ?>
-                                            @if(isset($sub_cat['product_inventory']))
-                                                <?php $inventory= $sub_cat['product_inventory']; ?>                                                
-                                                    <?php 
-//                                                    dd($inventory);
-                                                        $total_qnty=0;
-                                                        if(isset($inventory->physical_closing_qty) && isset($inventory->pending_purchase_advise_qty)){
-                                                            $total_qnty = $inventory->physical_closing_qty+$inventory->pending_purchase_advise_qty;
-                                                        }else{
-                                                            $total_qnty = "-";
-                                                        }                                                    
-                                                    ?>
-                                                    {{$total_qnty}}                                                
-                                            @endif 
-                                            </td>
+                                        </tr>                                        
+                                        @foreach($report_arr as $key=>$record)
+                                        <tr>                                            
+                                            <td>{{$key}}</td>                                                                                        
+                                            @if(isset($record))
+                                                @foreach($record as $value)
+                                                <td>    
+                                                   @if(isset($value))
+                                                        {{$value}}
+                                                   @else
+                                                        {{"-"}}     
+                                                   @endif
+                                                @endforeach
+                                                </td>
+                                            @endif                                             
                                         </tr>
                                         @endforeach                                        
                                     </tbody>
