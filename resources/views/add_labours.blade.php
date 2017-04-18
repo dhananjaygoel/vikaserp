8ceecd43e1a7d1bd2a5fa990078d479b449bffa9
atulahire <atula@agstech.co.in>
2017-04-18 17:36:27 +0530

    update: beta: curd labours: validation message with close button

--- resources/views/add_labours.blade.php
+++ resources/views/add_labours.blade.php
@@ -20,6 +20,7 @@
                             <input type="hidden" name="_token" value="{{csrf_token()}}">
                             @if (count($errors) > 0)
                             <div role="alert" class="alert alert-warning">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="position: relative;"><span aria-hidden="true">&times;</span></button>
                                 <ul>
                                     @foreach ($errors->all() as $error)
                                     <p>{{ $error }}</p>
