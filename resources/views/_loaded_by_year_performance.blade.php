00549896dc96cc3ae1587c82015150f07b8a7e53
Sharayu Naladkar <sharayun@agstechnologies.com>
2017-04-19 20:24:11 +0530

    update for performance

--- resources/views/_loaded_by_year_performance.blade.php
+++ resources/views/_loaded_by_year_performance.blade.php
@@ -1,50 +1,86 @@
+<?php
+//  
+
+$today = date("d", strtotime($enddate));
+if (date('m') == date("m", strtotime($enddate)) && date('y') == date("y", strtotime($enddate))) {
+    $today = date("d");
+}
+?> 
 <div class="table-responsive" >
     <table id="month-wise" class="table table-bordered complex-data-table">
         <tbody>
+            <?php
+//            $today = date("d", strtotime($enddate));
+
+            $month = date('m', strtotime($enddate));
+            if (date('y') == date("y", strtotime($enddate))) {
+                $month = date('m');
+            }
+            ?>
             <tr>
                 <td colspan="2" rowspan="1"></td>
-                <td colspan="31"><b>Month</b></td>
+                <td colspan="{{$month}}"><b>Month</b></td>
             </tr>
-            
             <tr class="text-bold">
                 <td colspan="2"></td>
-                <?php for ($m=1; $m<=12; $m++) {
-                    $month = date('F', mktime(0,0,0,$m, 1, date('Y'))); ?>
-                    <td>{{ $month }}</td>
-                <?php } ?>
+                @for($i = 1; $i<= $month ; $i++ )
+                <td>{{ date('F', mktime(0, 0, 0, $i)) }}</td>
+                @endfor
             </tr>
-            @if(isset($loaded_by))
-            
-                <?php 
-//                $date = date("Y-m",  strtotime($date));
-//                echo $date;
-//                $date_val = substr($date,0,8); 
-//                $date_val = substr($date,0,8); ?>
-                @foreach($loaded_by as $loader_val)
+            <?php foreach ($loaded_by as $loader) { ?>
                 <tr>
-                    <td rowspan="2"><b>{{$loader_val->first_name}}</B></td>
+                    <td rowspan="2"><b>{{$loader->first_name}} {{$loader->last_name}}</B></td>
                     <td><b>Tonnage</b></td>
-                    @for($i = 1; $i<= 12 ; $i++ )
-                        <?php 
-                        $k=0;
-                        $tangage=0;
-                            foreach ($final_array as $key => $value) {
-                                if($value['date']== $date){
-                                    if($value['loader_id'] == $loader_val->id){
-                                        $k++;
-                                        $tangage +=$value['tonnage'];
-                                    }
-                                }
+                    @for($i = 1; $i<= $month ; $i++ )
+                    <?php
+                    $k = 0;
+                    $tangage = 0;
+                    if ($i < 10) {
+                        $temp_month = '0' . $i;
+                    }
+                    $start_limit = '2017-' . $temp_month . '-1';
+                    $end_limit = '2017-' . $temp_month . '-31';
+
+                    foreach ($data as $key => $value) {
+                        if ($value['date'] >= $start_limit && $value['date'] <= $end_limit) {
+                            if ($value['loader_id'] == $loader->id) {
+
+                                $k++;
+                                $tangage +=$value['tonnage'];
                             }
-                        ?>
-                    <td>{{ number_format((float)$tangage, 2, '.', '') }}</td>
+                        }
+//                                                       
+                    }
+                    ?>
+                    <td>{{$tangage}}</td>
                     @endfor
-                    <tr>
-                        <td><b>Delivery</b></td>
-                    </tr>
                 </tr>
-                @endforeach
-            @endif
+                <tr>
+                    <td><b>Delivery</b></td>
+                    @for($i = 1; $i<= $month ; $i++ )
+                    <?php
+                    $k = 0;
+                    $tangage = 0;
+                    if ($i < 10) {
+                        $temp_month = '0' . $i;
+                    }
+                    $start_limit = '2017-' . $temp_month . '-1';
+                    $end_limit = '2017-' . $temp_month . '-31';
+
+                    foreach ($data as $key => $value) {
+                        if ($value['date'] >= $start_limit && $value['date'] <= $end_limit) {
+                            if ($value['loader_id'] == $loader->id) {
+
+                                $k++;
+                                $tangage +=$value['tonnage'];
+                            }
+                        }
+                    }
+                    ?>
+                    <td>{{$k}}</td>
+                    @endfor
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
 </div>
