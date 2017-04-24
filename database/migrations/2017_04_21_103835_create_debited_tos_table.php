8dd458843b1929f6553da350cb0d4603d7f96873
Sharayu Naladkar <sharayun@agstechnologies.com>
2017-04-24 15:31:22 +0530

    update migrations

---
+++ database/migrations/2017_04_21_103835_create_debited_tos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDebitedTosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('debited_tos', function(Blueprint $table)
+		{
+			$table->increments('id');
+                        $table->string('debited_to');
+                        $table->integer('debited_to_type')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('debited_tos');
+	}
+
+}
