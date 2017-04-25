93976d8bc059c8c10874d35fd249ae89a6a3f0ca
atulahire <atula@agstech.co.in>
2017-04-25 15:01:27 +0530

    update: inventory :excel

--- resources/views/excelView/inventory.blade.php
+++ resources/views/excelView/inventory.blade.php
@@ -16,16 +16,16 @@
         </tr>
         @foreach ($inventorys as $inventorylist)
         <tr>
-            <td>{{$inventorylist->product_sub_category->alias_name}}</td>
-            <td>{{$inventorylist->opening_qty}}</td>
-            <td>{{$inventorylist->sales_challan_qty}}</td>
-            <td>{{$inventorylist->purchase_challan_qty}}</td>
-            <td>{{$inventorylist->physical_closing_qty}}</td>
-            <td>{{$inventorylist->pending_sales_order_qty}}</td>
-            <td>{{$inventorylist->pending_delivery_order_qty}}</td>
-            <td>{{$inventorylist->pending_purchase_order_qty}}</td>
-            <td>{{$inventorylist->pending_purchase_advise_qty}}</td>
-            <td>{{$inventorylist->virtual_qty}}</td>
+            <td>{{isset($inventorylist['product_sub_category']->alias_name)?$inventorylist['product_sub_category']->alias_name:""}}</td>
+            <td>{{isset($inventorylist->opening_qty)?$inventorylist->opening_qty:""}}</td>
+            <td>{{isset($inventorylist->sales_challan_qty)?$inventorylist->sales_challan_qty:""}}</td>
+            <td>{{isset($inventorylist->purchase_challan_qty)?$inventorylist->purchase_challan_qty:""}}</td>
+            <td>{{isset($inventorylist->physical_closing_qty)?$inventorylist->physical_closing_qty:""}}</td>
+            <td>{{isset($inventorylist->pending_sales_order_qty)?$inventorylist->pending_sales_order_qty:""}}</td>
+            <td>{{isset($inventorylist->pending_delivery_order_qty)?$inventorylist->pending_delivery_order_qty:""}}</td>
+            <td>{{isset($inventorylist->pending_purchase_order_qty)?$inventorylist->pending_purchase_order_qty:""}}</td>
+            <td>{{isset($inventorylist->pending_purchase_advise_qty)?$inventorylist->pending_purchase_advise_qty:""}}</td>
+            <td>{{isset($inventorylist->virtual_qty)?$inventorylist->virtual_qty:""}}</td>
         </tr>
         @endforeach
     </table>
