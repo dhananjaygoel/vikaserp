10c7848111bfc065e612701dd36280bf8cd415a6
Sharayu Naladkar <sharayun@agstechnologies.com>
2017-04-26 12:03:16 +0530

    update receipts table

--- database/migrations/2017_04_21_104006_create_receipts_table.php
+++ database/migrations/2017_04_21_104006_create_receipts_table.php
@@ -5,28 +5,29 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateReceiptsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('receipts', function(Blueprint $table)
-		{
-			$table->increments('id')->comment('Primary key');                        
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        if (!Schema::hasTable('debited_tos')) {
+            Schema::create('receipts', function(Blueprint $table) {
+                $table->increments('id')->comment('Primary key');
+                $table->timestamps();
+            });
+        }
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-//		Schema::drop('receipts');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        if (Schema::hasTable('debited_tos')) {
+            Schema::drop('receipts');
+        }
+    }
 
 }
