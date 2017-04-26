6c7b9670d5a75089d28579b743a209a51de0b553
Sharayu Naladkar <sharayun@agstechnologies.com>
2017-04-26 11:08:20 +0530

    update for migration

--- database/migrations/2017_04_21_103835_create_debited_tos_table.php
+++ database/migrations/2017_04_21_103835_create_debited_tos_table.php
@@ -5,30 +5,31 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateDebitedTosTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('debited_tos', function(Blueprint $table)
-		{
-			$table->increments('id');
-                        $table->string('debited_to');
-                        $table->integer('debited_to_type')->unsigned();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        if (!Schema::hasTable('debited_tos')) {
+            Schema::create('debited_tos', function(Blueprint $table) {
+                $table->increments('id');
+                $table->string('debited_to');
+                $table->integer('debited_to_type')->unsigned();
+                $table->timestamps();
+            });
+        }
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-//		Schema::drop('debited_tos');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        if (Schema::hasTable('debited_tos')) {
+            Schema::drop('debited_tos');
+        }
+    }
 
 }
