49415e7235a12620c0d74abdb608e4262afef41e
Sujeet Koli <ksujeet@agstechnologies.com>
2017-04-27 15:10:55 +0530

    updated for customer filter

--- resources/views/customer_list.blade.php
+++ resources/views/customer_list.blade.php
@@ -42,10 +42,10 @@
                         </div> 
                         <div class="col-md-2 pull-right">  
                             @if(Auth::user()->role_id ==6 ||Auth::user()->role_id ==0)
-                            <select class="form-control" id="user_filter3" name="date_filter" onchange="this.form.submit();">
-                                <option value="1" >As of Today</option>
-                                <option value="3" >3 days</option>
-                                <option value="7" >A Week</option>
+                            <select class="form-control" id="date_filter" name="date_filter" onchange="this.form.submit();">
+                                <option value="1" <?php if(Input::get('date_filter')==1) echo "selected=''"; ?>>As of Today</option>
+                                <option value="3" <?php if(Input::get('date_filter')==3) echo "selected=''"; ?>>3 days</option>
+                                <option value="7" <?php if(Input::get('date_filter')==7) echo "selected=''"; ?>>A Week</option>
                             </select>
                             @endif
                         </div>
