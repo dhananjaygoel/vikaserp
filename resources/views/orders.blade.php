398b8df105c0262319c9c45fe825fe8706803fa2
atulahire <atula@agstech.co.in>
2017-05-11 20:11:33 +0530

    update:

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -454,8 +454,8 @@
                                                     <i class="fa fa-search fa-stack-1x fa-inverse"></i>
                                                 </span>
                                             </a>
-                                            @if( Auth::user()->role_id != 2 || Auth::user()->role_id != 3)
-                                            <a href="{{url('orders/'.$order->id.'/edit')}}" class="table-link" title="Edit{{Auth::user()->role_id}}">
+                                            @if( Auth::user()->role_id != 2 && Auth::user()->role_id != 3)
+                                            <a href="{{url('orders/'.$order->id.'/edit')}}" class="table-link" title="Edit">
                                                 <span class="fa-stack">
                                                     <i class="fa fa-square fa-stack-2x"></i>
                                                     <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
