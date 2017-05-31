0297675aa0c39593302b49762ffd4142d8b38ea5
atulahire <atula@agstech.co.in>
2017-05-31 21:45:25 +0530

    update: seeder for staging.vikaserp.in

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
 //		Model::unguard();
-//        $this->call('UserSeeder');
+        $this->call('UserSeeder');
         $this->call('UserRolesSeeder');
-//        $this->call('ProductTypeSeeder');
-//        $this->call('UnitsSeeder');
+        $this->call('ProductTypeSeeder');
+        $this->call('UnitsSeeder');
     }
 
 }
