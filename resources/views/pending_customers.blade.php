45514ae492f8bb434b6b40c208f8024e61f0ac04
atulahire <atula@agstech.co.in>
2017-06-15 18:36:55 +0530

    update: fixed:Add User ID in Pending Customer while deleting the customer

--- resources/views/pending_customers.blade.php
+++ resources/views/pending_customers.blade.php
@@ -94,15 +94,15 @@
                                                     <h4 class="modal-title" id="myModalLabel"></h4>
                                                 </div>
                                                 <div class="modal-body">
-                                                    <div class="delete">
-                                                        <div><b>UserID:</b> {{$c->username}}</div>
+                                                    <div class="delete"> 
+                                                        <div><b>UserID:</b> {{Auth::user()->mobile_number}}</div>
                                                         <div class="pwd">
                                                             <div class="pwdl"><b>Password:</b></div>
                                                             <div class="pwdr"><input class="form-control" placeholder="" name="password" type="password" type="text"></div>
 
                                                         </div>
                                                         <div class="clearfix"></div>
-                                                        <div class="delp">Are you sure you want to <b>cancel</b> this limit?</div>
+                                                        <div class="delp">Are you sure you want to <b>delete</b> this customer?</div>
                                                     </div>
                                                 </div>            
                                                 <div class="modal-footer">
