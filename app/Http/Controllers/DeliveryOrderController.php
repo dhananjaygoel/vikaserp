155896a6bd5cf4ed1cfe7edca96a6d201dfc479f
atulahire <atula@agstech.co.in>
2017-06-28 20:43:26 +0530

    update: tideway: DO index

--- app/Http/Controllers/DeliveryOrderController.php
+++ app/Http/Controllers/DeliveryOrderController.php
@@ -53,6 +53,7 @@ class DeliveryOrderController extends Controller {
      */
     public function index() {
         $data = Input::all();
+        gc_disable();
         if (Auth::user()->role_id != 0 && Auth::user()->role_id != 1 && Auth::user()->role_id != 2 && Auth::user()->role_id != 3 && Auth::user()->role_id != 4) {
             return Redirect::to('delivery_challan')->with('error', 'You do not have permission.');
         }
