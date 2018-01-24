04eb5cfb2458b3c9a06b1f7b57459398c6f49701
Sujeet Koli <ksujeet@agstechnologies.com>
2018-01-24 15:05:05 +0530

    updated for dc pdf

--- resources/views/delivery_challan_pdf.blade.php
+++ resources/views/delivery_challan_pdf.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
-<html>    
-    <body>
+<html>
+    <head>
+        <title>Delivery Challan </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
             body{
                 font-size: 10px;
@@ -201,6 +204,8 @@
             }
             
         </style>
+    </head>
+    <body>
         <div class="invoice">
             <div class="title">Estimate</div>
             <div class="name-date">
@@ -341,7 +346,7 @@
                         }
                         ?>
                         <td>{{ round($grand_price, 2) }}</td>
-                        </div>
+                    </tr>
                 </table>
             </div>
         </div>
