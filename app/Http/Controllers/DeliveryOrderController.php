cfca8bd3e5baf2b7b07fa6286aed2ed675094c73
janak.sensussoft@gmail.com <janak.sensussoft@gmail.com>
2019-03-29 14:56:54 +0530

    delivery order controller update for user type change

--- app/Http/Controllers/DeliveryOrderController.php
+++ app/Http/Controllers/DeliveryOrderController.php
@@ -57,6 +57,7 @@ class DeliveryOrderController extends Controller {
             return redirect('change_password');
         }
         gc_disable();
+        
         if (Auth::user()->role_id != 0 && Auth::user()->role_id != 1 && Auth::user()->role_id != 2 && Auth::user()->role_id != 3 && Auth::user()->role_id != 4 && Auth::user()->role_id != 8 && Auth::user()->role_id != 9) {
             return Redirect::to('delivery_challan')->with('error', 'You do not have permission.');
         }
