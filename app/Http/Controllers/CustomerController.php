a4c17cc140d0d296bffecbc7d04a37369f5dfb49
janak.sensussoft@gmail.com <janak.sensussoft@gmail.com>
2019-04-24 15:48:45 +0530

    quickbook token update

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -59,12 +59,9 @@ class CustomerController extends Controller {
      */
     public function index(Request $request) {
 
-       /* App\QuickbookToken::where('id',1)->update([
-            'client'=>'Q0exOpk5ncmFxPbI1HFYYXimHZm5H13bBQtJW9xzkiyKgHymL4',
-            'secret'=>'sPtMjs8xByXgxdG5IqgQqSAw94aPXmUOo8YAPU0R',
-            'access_token'=>'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..LE3U8OFZYNfbB839B5ooLw.f4bXS2caHyxR8IaM2QYaT4KKHjuSX0SqXp19mAtHXOG3HOsiBX3_yeRZ3Zjds_6pJVfcHcp4gTtkjAcjCx2qJFoxBMRL81yC0Cl-hBCK7buSTSkZ-YOH2KiR5e1rY5Nm2MNQ8sqk1tiyj9SgMfExK0wZFIwT3qcqLx1yUhBepDDkkA-27oe18NGkd6wKlAp4zzAOrq4h0Nr7_O-8cnMjW-VBzrew5mzwqrSUctrEZxahHPPQ8pvfJrCEjs_KbLttniZ-e8ULOu6DsDRLNl8QMMKIBGNdJAIG5ad0Fkhjw7EJkg8VleMlLfPmR4d76nhlR7sEPydntMtT1rnwKk5aDjMUxVVYLhJnBn1z0oOSq_UxMl2hMmQ6cbtuL2j6djqQ-tHh6O-TqVeAHHJbAYXHYLwmqEexGMfM3Y5A0Q_11MAXLmjWg8v2CuOP8eDJMYKYA14svAJFzZvg0W5i8wZAeswmtjOUouoN1C3bbEqOwhAud0mfeU7WSTQ5Do_v9DsawZvvOu8yNUBfWrfgybg8p05kvjfzgqpxhT6aEZLtsmB5O_130Qe2sBm9R2UjiACrYDVbzNioSMR3bPSbOCLhKZRrPt3ZhxPeBZ8r-VHffFB3XkdCvhlAWWesGlIxQIhaxwUx9qEIsW4s3qB_xBDUnn4eWgCQxYZ-8136Y3xXbfTQyn2e-9Nmbu9nBHMj23_e.4R3-nCL7EJH0x8KpPj0R7w',
-            'refresh_token'=>'L011564631964o65eQXYx2KTBqNWTXPnLIGvct6FXIhm5O1UIv'
-        ]);*/
+        App\QuickbookToken::where('id',1)->update([
+            'refresh_token'=>'Q011564811807Zwx0xhCGlcy2tlQqn2FdXczw2kh9uIKY2dL7H',
+        ]);
 
         if (Auth::user()->hasOldPassword()) {
             return redirect('change_password');
