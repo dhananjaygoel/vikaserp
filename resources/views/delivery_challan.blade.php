76f1dabe4e5c67955576be83741db6b917a1af1b
janak.sensussoft@gmail.com <janak.sensussoft@gmail.com>
2019-04-25 11:49:12 +0530

    update invoice button in challan

--- resources/views/delivery_challan.blade.php
+++ resources/views/delivery_challan.blade.php
@@ -177,6 +177,13 @@
                                                 }
                                             ?>
 
+                                            <span class="table-link normal_cursor" title="Generate Invoice">
+                                               <span class="fa-stack">
+                                                        <i class="fa fa-square fa-stack-2x"></i>
+                                                        <i class="fa fa-file-text fa-stack-1x fa-inverse"></i>
+                                               </span>
+                                            </span>
+
                                             <a href="" class="table-link" title="print" data-toggle="modal" data-target="#print_challan" onclick="print_delivery_challan({{$challan->id}},{{$is_allinc}})">
                                                 <span class="fa-stack">
                                                     <i class="fa fa-square fa-stack-2x"></i>
@@ -184,12 +191,7 @@
                                                 </span>
                                             </a>
 
-                                            <span class="table-link normal_cursor" title="Generate Invoice">
-                                               <span class="fa-stack">
-                                                        <i class="fa fa-square fa-stack-2x"></i>
-                                                        <i class="fa fa-file-text fa-stack-1x fa-inverse"></i>
-                                               </span>
-                                            </span>
+
 
                                             @if( Auth::user()->role_id == 0  || Auth::user()->role_id == 1)
                                             <a href="#" class="table-link danger" data-toggle="modal" data-target="#delete_challan" title="delete" onclick="delete_challan({{$challan->id}})">
