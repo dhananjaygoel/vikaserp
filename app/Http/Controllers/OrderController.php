5ec7b3cee4274d5a93c92edf7ec7c241e6d3568f
vbadheli <vbadheli@gmail.com>
2019-05-28 14:09:05 +0530

    order page test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -295,8 +295,8 @@ class OrderController extends Controller {
         $delivery_location = DeliveryLocation::orderBy('area_name', 'ASC')->get();
         $delivery_order = AllOrderProducts::where('order_type', '=', 'delivery_order')->where('product_category_id', '=', $product_category_id)->get();
 
+        dd($delivery_order);
         $product_size = ProductSubCategory::all();
-        dd("ii");
         $pending_orders = $this->checkpending_quantity($allorders);
         $allorders->setPath('orders');
 //        $non_approved_orders = Order::with('all_order_products', 'customer', 'delivery_location', 'createdby')
