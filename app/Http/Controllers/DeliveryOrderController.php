6d556e9eb59d6a78c982cbdfd64dfa0a91e6928f
vbadheli <vbadheli@gmail.com>
2019-05-31 13:25:33 +0530

    print_delivery_order fix it

--- app/Http/Controllers/DeliveryOrderController.php
+++ app/Http/Controllers/DeliveryOrderController.php
@@ -1387,13 +1387,13 @@ class DeliveryOrderController extends Controller {
             }
         }
 // dd(getcwd());
-        Storage::put("/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', $pdf->output());
-        $pdf->save( "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf');
-        chmod( "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', 0777);
+        // Storage::put("/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', $pdf->output());
+        // $pdf->save( "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf');
+        // chmod( "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', 0777);
 
-        // Storage::put(getcwd() . "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', $pdf->output());
-        // $pdf->save(getcwd() . "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf');
-        // chmod(getcwd() . "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', 0777);
+        Storage::put(getcwd() . "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', $pdf->output());
+        $pdf->save(getcwd() . "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf');
+        chmod(getcwd() . "/upload/invoices/do/" . str_replace('/', '-', $date_letter) . '.pdf', 0777);
 
         //$connection->getConnection()->put('Delivery Order/' . date('d-m-Y') . '/' . str_replace('/', '-', $date_letter) . '.pdf', $pdf->output());
 
