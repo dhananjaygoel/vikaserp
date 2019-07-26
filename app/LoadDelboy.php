6b3073fa9cf715c6518abb4efd3e6b80b8be3bea
Saranya Anuraj <saranya@syoware.com>
2019-07-26 18:45:00 +0530

    added two tables

---
+++ app/LoadDelboy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LoadDelboy extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'truckdelbys';
+
+    
+   
+    
+ 
+
+}
---
+++ app/LoadTrucks.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LoadTrucks extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'load_truck';
+
+    
+   
+    
+ 
+
+}
