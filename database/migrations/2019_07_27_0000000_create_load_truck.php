cadc083c51f936e6711d186dd5757006d9cc4244
Saranya Anuraj <saranya@syoware.com>
2019-07-27 00:55:13 +0530

    migration added load_truck table

---
+++ database/migrations/2019_07_27_0000000_create_load_truck.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoadTruckTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('load_truck', function(Blueprint $table)
+		{
+			$table->increments('id');
+                        $table->integer('role_id');
+                        $table->integer('deliver_id');
+                        $table->integer('empty_truck_weight');
+                        $table->integer('final_truck_weight');
+                        $table->integer('userid');
+			
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('load_truck');
+	}
+
+}
