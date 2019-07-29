a2f41ba5bbc8bf98d3375674742cb4971b66e42d
Saranya Anuraj <saranya@syoware.com>
2019-07-29 17:07:05 +0530

    class name changed

--- database/migrations/2019_07_27_0000000_create_load_truck.php
+++ database/migrations/2019_07_27_0000000_create_load_truck.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLoadTruckTable extends Migration {
+class CreateLoadTruck extends Migration {
 
 	/**
 	 * Run the migrations.
