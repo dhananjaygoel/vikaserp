0e453ed2ab1e3b830039649fbd721fd1b940b1a8
Saranya Anuraj <saranya@syoware.com>
2019-08-01 16:44:50 +0530

    some little database change

--- database/migrations/2019_08_01_043200_update_delivery_challan_table.php
+++ database/migrations/2019_08_01_043200_update_delivery_challan_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateDeliveryChallan1Table extends Migration {
+class UpdateDeliveryChallanTable extends Migration {
 
     /**
      * Run the migrations.
