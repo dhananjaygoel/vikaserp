4836fc6b011ae594c656d2b9d3c821605c7fbd91
Saranya Anuraj <saranya@syoware.com>
2019-08-01 17:47:15 +0530

    database file updated

--- database/migrations/2019_08_01_053600_alter_delivery_challan_table.php
+++ database/migrations/2019_08_01_053600_alter_delivery_challan_table.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateDeliveryChallanTable extends Migration {
+class AlterDeliveryChallanTable extends Migration {
 
     /**
      * Run the migrations.
