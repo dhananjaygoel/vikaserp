17dd5fd7f1c9ac6d8c7e6ff863b1133bab9584fc
Ganesh Perumal <ganesh@slicecarving.com>
2019-08-10 10:54:45 +0530

    auth

--- app/Http/Middleware/validIpMiddleware.php
+++ app/Http/Middleware/validIpMiddleware.php
@@ -41,7 +41,7 @@ class validIpMiddleware {
                 $ipaddress = 'UNKNOWN';
             if ($ipaddress != 'UNKNOWN') {
                 //  && Auth::user()->role_id != 2
-                if (!in_array($ipaddress, $ip_array) && Auth::user()->role_id != 0 && Auth::user()->role_id != 1 && Auth::user()->role_id != 5 && Auth::user()->role_id != 8 && Auth::user()->role_id != 9) {
+                if (!in_array($ipaddress, $ip_array) && Auth::user()->role_id != 0 && Auth::user()->role_id != 1 && Auth::user()->role_id != 5 && Auth::user()->role_id != 8 && Auth::user()->role_id != 9 && Auth::user()->role_id != 2) {
                     return redirect('dashboard');
                 }
 
