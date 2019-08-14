179153c468a7ca380121afc931dd788aee23d04f
Saranya Anuraj <saranya@syoware.com>
2019-08-14 15:56:51 +0530

    new migration file added

--- database/migrations/2019_08_14_131035_update_delivery_challan_table_new.php
+++ database/migrations/2019_08_14_131035_update_delivery_challan_table_new.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateDeliveryChallanNew extends Migration {
+class UpdateDeliveryChallanTableNew extends Migration {
 
 	/**
 	 * Run the migrations.
