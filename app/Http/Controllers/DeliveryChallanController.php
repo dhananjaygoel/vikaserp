2ff90dd0b172fe4e545b47b03c95725b5789404c
Saranya Anuraj <saranya@syoware.com>
2019-08-14 18:22:22 +0530

    little bit changes had been added in delvery challan controller for generating invoices

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -1112,7 +1112,74 @@ class DeliveryChallanController extends Controller {
                     ]
                 ];
             }
-            print_R($line);
+            if($del_products->vat_percentage==0)
+            {
+                $quickbook_customer_id=$update_delivery_challan->customer->quickbook_a_customer_id;                   
+            }
+            else
+            {
+                $quickbook_customer_id=$update_delivery_challan->customer->quickbook_customer_id;
+            } 
+             $theResourceObj = Invoice::create([
+                "Line" => $line,
+                "CustomerRef"=> [
+                    // "value"=> $update_delivery_challan->customer->quickbook_customer_id,
+                    "value"=> $quickbook_customer_id,
+                ],
+                // 'GlobalTaxCalculationEnum'=>'NotApplicable'
+            ]);
+          $inv = $dataService->add($theResourceObj);
+           $error = $dataService->getLastError();
+            if ($error) {  
+            if($del_products->vat_percentage==0)
+                {
+                    $this->refresh_token_Wihtout_GST();
+                    $dataService = $this->getTokenWihtoutGST(); 
+                    // $inv = $dataService->add($theResourceObj);                   
+                }
+                else{
+                    $this->refresh_token();
+                    $dataService = $this->getToken();
+
+                }              
+                $inv = $dataService->add($theResourceObj);    
+                // echo '<pre>';
+                // print_r($inv);
+                // exit;            
+                $error1 = $dataService->getLastError();
+                // echo '<pre>';
+                // print_r($theResourceObj);
+                // echo '<br>';
+                // echo '<br>';
+                // echo '<br>';
+                // print_r($inv);
+                // echo '<br>';
+                // echo '<br>';
+                // echo '<br>';
+                // print_r($error1);
+                // exit;
+                if($error1){
+                    echo "The Status code is: " . $error->getHttpStatusCode() . "\n";
+                    echo "The Helper message is: " . $error->getOAuthHelperError() . "\n";
+                    echo "The Response message is: " . $error->getResponseBody() . "\n";
+                    die;
+                }
+                else{
+                    $doc_num =  $inv->Id;
+                }
+            }
+            else {
+                $doc_num =  $inv->Id;
+            }
+            DeliveryChallan::where('id',$id)->update(['doc_number'=>$doc_num]);
+            if(Auth::user()->role_id != 0){
+                DeliveryChallan::where('id',$id)->update(['is_print_user'=>1]);
+            }
+
+           // $invoice = $dataService->Query("select * from Invoice where id = '".$doc_num."' ");
+            $pdf = $dataService->DownloadPDF($inv,base_path('upload/invoice/'));
+            $pdfNAme = explode('invoice/',$pdf)[1];
+            return redirect()->away(asset('upload/invoice/'.$pdfNAme));
         }
     }
 
