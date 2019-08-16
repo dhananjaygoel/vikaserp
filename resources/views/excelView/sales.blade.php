b60842338fa38953e05190a2c65942f2c7473d93
Saranya Anuraj <saranya@syoware.com>
2019-08-16 18:31:31 +0530

    For All inclusive orders add an option to export in excel format compatible with Quickbooks

--- resources/views/excelView/sales.blade.php
+++ resources/views/excelView/sales.blade.php
@@ -76,15 +76,16 @@
                     <td>{{ $value['date'] }}</td>     
                     <td>{{ $value['date'] }}</td>  
                     <td>Net 30</td>
-                    <td>Memo</td>       
-                    <td>{{ $value['no'] }}</td>       
-                    <td>{{ $value['placeof_supply'] }}</td>       
+                    <td>Memo</td>    
+                    <td>{{ $value['placeof_supply'] }}</td>      
+                    <td>{{ $value['no'] }}</td>  
                     <td>{{ $value['due_date'] }}</td>                    
                     <td>{{ $value['balance'] }}</td>       
                     <td>{{ $value['total_btax'] }}</td>    
                     <td>{{ $value['tax'] }}</td>    
                     <td>{{ $value['total'] }}</td>    
                     <td>{{ $value['status'] }}</td>       
+                    <td>{{ $value['due_date'] }}</td>   
                 </tr>
             <?php
             $VchNo++;
