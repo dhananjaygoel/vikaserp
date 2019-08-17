b59dae4597d160d462f8b9472b0b9e416f39cbd3
Saranya Anuraj <saranya@syoware.com>
2019-08-17 16:02:55 +0530

    truck weight loaded by delivery boy name

--- resources/views/create_load_truck.blade.php
+++ resources/views/create_load_truck.blade.php
@@ -133,7 +133,7 @@
                          
                         @foreach($delboy as $key => $info) 
               <?php
-              print_R($info);
+              print_R($delivery_data);
                             if($key ==0){
                                 $label = 1;
                             }
