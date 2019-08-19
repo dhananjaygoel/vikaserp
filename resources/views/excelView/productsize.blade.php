6d12989f0e09a5eed1fb333e0052c628ddb849f1
Saranya Anuraj <saranya@syoware.com>
2019-08-19 17:05:20 +0530

    alter product export list

--- resources/views/excelView/productsize.blade.php
+++ resources/views/excelView/productsize.blade.php
@@ -4,19 +4,18 @@
 
     <table>
         <tr>
-            <td class="heading1">Category Name</td>
-            <td class="heading1">Price</td>
-            <td class="heading1">Unit</td>
-            <td class="heading1">Alias Name</td>
-            <td class="heading1">Size</td>
-            <td class="heading1">Weight</td>
-            <td class="heading1">Thickness</td>
-            <td class="heading1">Standard Length</td>
-            <td class="heading1">Difference</td>
+            <td class="heading1">Product/Service Name</td>
+            <td class="heading1">Sales Description</td>
+            <td class="heading1">Sales Price / Rate</td>
+            <td class="heading1">Purchase Cost</td>
+            <td class="heading1">HSN/SAC</td>
+            <td class="heading1">Type</td>
+            
         </tr>
         @foreach ($product_size_list as $key => $value)
         <tr>
-            <td>{{$value->product_category->product_category_name}}</td>
+            <td>{{$value->alias_name}}</td>
+            <td>{{$value->alias_name}}</td>
             <td>
                 @if(substr($value->difference, 0, 1) == '-')
                 {{ $value->product_category->price - substr($value->difference,1) }}
@@ -24,13 +23,16 @@
                 {{ $value->product_category->price + $value->difference }}
                 @endif
             </td>
-            <td>{{$value->product_unit->unit_name}}</td>
-            <td>{{$value->alias_name}}</td>
-            <td>{{$value->size}}</td>
-            <td>{{$value->weight}}</td>
-            <td>{{$value->thickness}}</td>
-            <td>{{$value->standard_length}}</td>
-            <td>{{$value->difference}}</td>
+            <td>
+                @if(substr($value->difference, 0, 1) == '-')
+                {{ $value->product_category->price - substr($value->difference,1) }}
+                @else
+                {{ $value->product_category->price + $value->difference }}
+                @endif
+            </td>
+            <td>{{$value->hsn_code}}</td>
+            <td>Noninventory</td>
+         
         </tr>
         @endforeach
     </table>
