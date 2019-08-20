cf05d00794240afd73003035ea37dfaa90f679af
Saranya Anuraj <saranya@syoware.com>
2019-08-20 17:10:33 +0530

    alter delivery challan for generating invoices

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -1250,7 +1250,7 @@ class DeliveryChallanController extends Controller {
                 $tally_name = $update_delivery_challan->customer->tally_name;      
             } 
             //print_R($line);
-            $customer_details = $dataService->Query("select * from Account");
+            $customer_details = $dataService->Query("select * from Customer");
             print "hi";
             print_R($customer_details);
            /*
