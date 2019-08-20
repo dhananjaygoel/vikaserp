19b06baea75fe94142631f4efe773483a9c97832
Saranya Anuraj <saranya@syoware.com>
2019-08-20 12:31:46 +0530

    alter challan controller to generate invoice

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -1090,9 +1090,6 @@ class DeliveryChallanController extends Controller {
                 $pdf = $dataService->DownloadPDF($invoice[0],base_path('upload/invoice/'));
             }
             else{
-                print_R($invoice); print $update_delivery_challan->doc_number;
-                                DeliveryChallan::where('id',$id)->update(['doc_number'=>""]);print "ggg";
-
                 $pdf = $dataService->DownloadPDF($invoice[0],base_path('upload/invoice/'));
             }
             $pdfNAme = explode('invoice/',$pdf)[1];
