1deb1e5d416b29163d193036ed61361fe81b2d81
Saranya Anuraj <saranya@syoware.com>
2019-08-21 16:59:14 +0530

    Delivery Staff should be able to edit rate of individual products

--- resources/views/create_load_truck.blade.php
+++ resources/views/create_load_truck.blade.php
@@ -156,7 +156,7 @@
               $label =" loaded by ".$owner_name;
              ?>
                         <div class ="row form-group">
-                        <span class="col-md-2"> Truck Weight(Kg):</span>
+                        <span class="col-md-6"> Truck Weight(Kg):</span>
              <span class="col-md-8">
                         @if($info->del_boy == Auth::id() )
                         
@@ -164,9 +164,8 @@
                          <input type="text" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control " name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > 
                          @else
                           <input type="text" readonly="readonly" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control" name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > 
-                          <span class="col-md-2">{{$label}}</span>
                           </span>
-                           
+                           <span class="col-md-2">{{$label}}</span>
                             
                           
                           </div>
