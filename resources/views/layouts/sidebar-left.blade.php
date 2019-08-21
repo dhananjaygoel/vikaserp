e66ec9dcec1d870c4a6495f856382cc6a033da5c
Saranya Anuraj <saranya@syoware.com>
2019-08-21 13:10:15 +0530

    sales staff should not edit delivery order

--- resources/views/layouts/sidebar-left.blade.php
+++ resources/views/layouts/sidebar-left.blade.php
@@ -197,7 +197,7 @@
                             </li>
 
                             @endif
-                            @if(Auth::user()->role_id == 9 ||  Auth::user()->role_id == 2 || Auth::user()->role_id == 8 || Auth::user()->role_id == 0 ||Auth::user()->role_id == 1 || Auth::user()->role_id == 2 || Auth::user()->role_id == 3 || Auth::user()->role_id == 4 )
+                            @if(Auth::user()->role_id == 9 ||  Auth::user()->role_id == 2 || Auth::user()->role_id == 8 || Auth::user()->role_id == 0 ||Auth::user()->role_id == 1  || Auth::user()->role_id == 3 || Auth::user()->role_id == 4 )
                                 @if(Auth::user()->role_id != 8 && Auth::user()->role_id != 9)
                                 <li class="{{ (Request::is('*orders*') ? 'active' : '') }}">
                                     <a href="{{url('orders')}}" >
