362bb88c0df0ad1955a1d9e71885e0ff59b6259f
Saranya Anuraj <saranya@syoware.com>
2019-08-21 16:48:20 +0530

    Delivery Staff should be able to edit rate of individual products

--- resources/views/create_load_truck.blade.php
+++ resources/views/create_load_truck.blade.php
@@ -157,13 +157,14 @@
              ?>
                         <div class ="row form-group">
                         <span class="col-md-2"> Truck Weight(Kg):</span>
-            
+             <span class="col-md-2">
                         @if($info->del_boy == Auth::id() )
                         
-                        <span class="col-md-2">
-                         <input type="text" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control " name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > </span>
+                       
+                         <input type="text" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control " name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > 
                          @else
-                          <span class="col-md-2"><input type="text" readonly="readonly" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control" name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > </span>
+                          <input type="text" readonly="readonly" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control" name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > 
+                          </span>
                            <span class="col-md-2">{{$label}}</span>
                             <span class="col-md-6"></span>
                           
