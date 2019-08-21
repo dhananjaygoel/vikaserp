a03eb3422c10a555cbaf8975b2fe5adda9027de6
Saranya Anuraj <saranya@syoware.com>
2019-08-21 16:45:55 +0530

    Delivery Staff should be able to edit rate of individual products

--- resources/views/create_load_truck.blade.php
+++ resources/views/create_load_truck.blade.php
@@ -160,12 +160,13 @@
             
                         @if($info->del_boy == Auth::id() )
                         
-                          <span class="col-md-2">
-                         <input type="text" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control " name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" >
+                        <span class="col-md-2">
+                         <input type="text" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control " name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > </span>
                          @else
-                          <input type="text" readonly="readonly" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control" name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > </span>
+                          <span class="col-md-2"><input type="text" readonly="readonly" name="truck_weight{{$info->del_boy}}" value="{{$tvalue}}" id="truck_weight{{$info->del_boy}}" class="form-control" name="truck_weight{{$info->del_boy}}" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" > </span>
                            <span class="col-md-2">{{$label}}</span>
-                            
+                            <span class="col-md-6"></span>
+                          
                           </div>
                           @endif  
                          
