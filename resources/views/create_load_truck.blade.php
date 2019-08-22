db6128b6519770910841eb4d4617221ab18e7540
Saranya Anuraj <saranya@syoware.com>
2019-08-23 00:52:56 +0530

    Empty truck weight can be edited by all

--- resources/views/create_load_truck.blade.php
+++ resources/views/create_load_truck.blade.php
@@ -93,8 +93,8 @@
                             <span class="col-md-2">Empty Truck Weight(Kg):</span> 
                             @if(isset($delivery_data->empty_truck_weight))
                             @if($delivery_data->empty_truck_weight > 0)
-                            {{ $delivery_data->empty_truck_weight}}
-                            <input type="hidden" name="empty_truck_weight" value="{{isset($delivery_data->empty_truck_weight)?$delivery_data->empty_truck_weight:'0'}}" id="empty_truck_weight" class="form-control" name="empty_truck_weight">
+                            
+                            <input type="text" name="empty_truck_weight" value="{{isset($delivery_data->empty_truck_weight)?$delivery_data->empty_truck_weight:'0'}}" id="empty_truck_weight" class="form-control" name="empty_truck_weight"onkeypress=" return numbersOnly(this, event, false, false);">
                             @else
                             <input type="text" name="empty_truck_weight" value="{{isset($delivery_data->empty_truck_weight)?$delivery_data->empty_truck_weight:'0'}}" id="empty_truck_weight" class="form-control" name="empty_truck_weight" style="width: 10.33%;" maxlength="10" onkeypress=" return numbersOnly(this, event, false, false);" >
                             @endif
