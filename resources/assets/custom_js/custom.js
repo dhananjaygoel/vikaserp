f4bfa5ca0977b0ed519f52ce6a08951d3f9a66bc
Saranya Anuraj <saranya@syoware.com>
2019-08-22 15:27:00 +0530

    sales staff should be ableto assign supervisor

--- resources/assets/custom_js/custom.js
+++ resources/assets/custom_js/custom.js
@@ -3252,7 +3252,7 @@ function loaded_truck(order_id) {
 }
 function order_assign(){
     var delivery_id = $(this).attr('data-delivery_id');
-    consol.log(delivery_id);
+    console.log(delivery_id);
     var del_supervisor =$("#del_supervisor").val(); 
     var token = $('#_token').val();
     if(del_supervisor){
