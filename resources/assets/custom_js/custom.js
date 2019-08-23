ff8d95bd647ebc4aa19cd2ffc257a211c370cf0c
Saranya Anuraj <saranya@syoware.com>
2019-08-23 11:42:38 +0530

    delivery staff should be able to assign supervisor

--- resources/assets/custom_js/custom.js
+++ resources/assets/custom_js/custom.js
@@ -3303,7 +3303,8 @@ function order_assign(){
 function loaded_assign(){
     var delivery_id = $("#delivery_id").val();
     var assigntype = $("#assign_type").val();
-    var del_supervisor =$("#del_supervisor").val(); 
+    var del_supervisor =$("#assigndel_supervisor").val(); 
+    console.log(del_supervisor);
     var token = $('#_token').val();
     if(del_supervisor){
         
--- resources/views/delivery_order.blade.php
+++ resources/views/delivery_order.blade.php
@@ -629,8 +629,8 @@
                     
         
         ?>
-                <select  class="form-control del_supervisor" name="del_supervisor"  data-order_id="{{$delivery->order_id}}"  data-role_id="{{$roleid}}" data-supervisor_id="{{$delivery->del_supervisor}}"
-                 data-delivery_boy="{{$delivery->del_boy}}" data-delivery_id="{{$delivery->id}}" id="del_supervisor"> 
+                <select  class="form-control assigndel_supervisor" name="del_supervisor"  data-order_id="{{$delivery->order_id}}"  data-role_id="{{$roleid}}" data-supervisor_id="{{$delivery->del_supervisor}}"
+                 data-delivery_boy="{{$delivery->del_boy}}" data-delivery_id="{{$delivery->id}}" id="assigndel_supervisor"> 
                                                         @foreach($options as $optkey =>$user)
                                                               <option value = {{$optkey }}>{{$user}}</option>  
                                                         @endforeach
