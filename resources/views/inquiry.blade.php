ac1b59de93b020a9a20ec0dc1403349e2194eacb
Saranya Anuraj <saranya@syoware.com>
2019-08-23 16:44:25 +0530

    remove duplicate inquiry

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -128,8 +128,7 @@
 
                                 
                                     @foreach($inquiries as $inquiry)
-                                   <?php $product_details = $inquiry['inquiry_products']->getRelation('inquiry_product_details'); ?>
-                                    <?php print_r($product_details);?>
+                                  
                                     <tr id="inquiry_row_{{$inquiry['id']}}">
                                         <td class="text-center">{{$i++}}</td>
                                         <td class="text-center">
