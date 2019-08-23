a57b7970539cc3e60951b3041d4c4a95b960f6c2
Saranya Anuraj <saranya@syoware.com>
2019-08-23 16:34:46 +0530

    remove duplicate inquiry

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -128,7 +128,7 @@
 
                                 
                                     @foreach($inquiries as $inquiry)
-                                    <?php print_r($inquiry['inquiry_products']['items:protected'] );?>
+                                    <?php print_r($inquiry['inquiry_products']);?>
                                     <tr id="inquiry_row_{{$inquiry['id']}}">
                                         <td class="text-center">{{$i++}}</td>
                                         <td class="text-center">
