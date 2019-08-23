444c737a14761326204df925ee95a8a04da7e296
Saranya Anuraj <saranya@syoware.com>
2019-08-23 16:24:00 +0530

    remove duplicate inquiry

--- resources/views/inquiry.blade.php
+++ resources/views/inquiry.blade.php
@@ -123,7 +123,7 @@
                                 </thead>
                                 <tbody>
                                     <?php $i = ($inquiries->currentPage() - 1) * $inquiries->perPage() + 1; 
-                                     print_r($inquiries['[inquiry_products]']);
+                                     print_r($inquiries['inquiry_products']);
                                      ?>
 
                                 
