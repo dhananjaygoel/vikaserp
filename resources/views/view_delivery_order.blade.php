d24aee3ff6ca49f67f99f203913c86bd6e7f16f5
Saranya Anuraj <saranya@syoware.com>
2019-08-23 17:42:29 +0530

    Remove 'Edit Delivery Order' option fromDelivery Supervisor and Delivery Boy

--- resources/views/view_delivery_order.blade.php
+++ resources/views/view_delivery_order.blade.php
@@ -12,7 +12,7 @@
                 <div class="filter-block">
                     <h1 class="pull-left">View Delivery Order</h1>
                     <div class="pull-right top-page-ui">
-                     
+                     <?php print Auth::user()->role_id; ?>
                         @if($delivery_data->order_status == 'pending')
                         @if($delivery_data->serial_no == "" || Auth::user()->role_id == 0  || Auth::user()->role_id == 1 || Auth::user()->role_id !=9 )
                         <a href="{{URL::action('DeliveryOrderController@edit',['id'=>$delivery_data->id])}}" class="btn btn-primary pull-right">
