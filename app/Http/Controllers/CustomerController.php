3a98d617dadcfb7df45a55076c284d0a143a9ede
Saranya Anuraj <saranya@syoware.com>
2019-08-30 16:36:03 +0530

    add customer in quickbook account

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -239,7 +239,7 @@ class CustomerController extends Controller {
             'ClientSecret' => $quickbook->secret,
             'accessTokenKey' =>  $quickbook->access_token,
             'refreshTokenKey' => $quickbook->refresh_token,
-            'QBORealmID' => "9130346686579506",
+            'QBORealmID' => "9130346851577266",
             'baseUrl' => "production"));
     //    return $dataService = \QuickBooksOnline\API\DataService\DataService::Configure(array(
     //         'auth_mode' => 'oauth2',
