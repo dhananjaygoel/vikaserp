4e0c8e06f0171a961a5386ee323760be9e1abe12
Saranya Anuraj <saranya@syoware.com>
2019-08-31 16:02:24 +0800

    correcting delivery challan

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -1168,7 +1168,7 @@ class DeliveryChallanController extends Controller {
                     ]
                 ];
              }
-            
+             print_R($line);
             if($del_products->vat_percentage==0)
             {
                 $quickbook_customer_id=$update_delivery_challan->customer->quickbook_a_customer_id;
