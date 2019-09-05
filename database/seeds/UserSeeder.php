f07cf0d5a059e4fa6366980ee2f927a1c2369d24
Saranya Anuraj <saranya@syoware.com>
2019-09-05 15:01:08 +0530

    category  name changed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -6,12 +6,19 @@ use Illuminate\Support\Facades\DB;
 class UserSeeder extends Seeder {
 
     public function run() {
-       $subcategories = App\ProductSubCategory::all();
+       /*$subcategories = App\ProductSubCategory::all();
        foreach ($subcategories as $subcat) {
         $hsn = App\ProductCategory::where('id', $subcat->product_category_id)->get()->first();
          DB::table('product_sub_category')->where('product_category_id', $subcat->product_category_id)->update([
                 'hsn_code' => $hsn->hsn_code,
-            ]);
+            ]);*/
+             DB::table('product_type')->truncate();
+        DB::table('product_type')->insert(
+                array(
+                    array('id' => '1','name' =>'Pipe' ),
+                    array('id' => '2','name' =>'Structure'),
+                    array('id' => '3','name' =>'Sheets')
+                ));
        }
 
     }
