09df3f452c8385cfb384f01170c30e5e56d75f53
Saranya Anuraj <saranya@syoware.com>
2019-09-05 13:04:56 +0530

    update hsn code;

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,13 +11,12 @@ class UserSeeder extends Seeder {
                 array(
                     //Super Admin
                     array(
-                        'id' => '10',
+                        'id' => '1',
                         'first_name' => 'Super',
                         'last_name' => 'Admin',
                         'phone_number' => '1234567890',
                         'mobile_number' => '9898989890',
-                        'email' => 'sadmin@admin.com',
-                        // 'password' => '$2y$10$vkxXDsOalnfS.ANuYhDIkunjjtZGzvVD497Tvl0/vCnBAMKxkcaYC',
+                        'email' => 'saranyanuraj@gmail.com',
                         'password' => '$2y$10$jpxgWw.w0OgC6yT2/DIycOO/VeeNfxus6FFiZuiglNcPAtirgDTOm',
                         'role_id' => '0'
                     ),
