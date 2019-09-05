22ad9b2ff1c9b8e2d2da5de48e23c55a412e3ed0
Saranya Anuraj <saranya@syoware.com>
2019-09-05 12:45:22 +0530

    update hsn code;

---
+++ database/seeds/HsnUpdateSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class HsnUpdateSeeder extends Seeder {
+	  public function run() {
+        DB::table('users')->truncate();
+        DB::table('users')->insert(
+                array(
+                    //Super Admin
+                    array(
+                        'id' => '1',
+                        'first_name' => 'Super',
+                        'last_name' => 'Admin',
+                        'phone_number' => '1234567890',
+                        'mobile_number' => '9898989890',
+                        'email' => 'sadmin@admin.com',
+                        // 'password' => '$2y$10$vkxXDsOalnfS.ANuYhDIkunjjtZGzvVD497Tvl0/vCnBAMKxkcaYC',
+                        'password' => '$2y$10$jpxgWw.w0OgC6yT2/DIycOO/VeeNfxus6FFiZuiglNcPAtirgDTOm',
+                        'role_id' => '0'
+                    ),
+        ));
+    }
+
+}
+
