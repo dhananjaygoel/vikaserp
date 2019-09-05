f5bbb903b4d022e1e224f7f12f78fc9882eb0639
Rakshiya <rakshiyas@agstech.co.in>
2019-09-05 16:43:03 +0530

    fix:validation on unit dropdown.

--- resources/views/edit_order.blade.php
+++ resources/views/edit_order.blade.php
@@ -282,7 +282,7 @@
                                                         <td class="col-md-2">
                                                             <div class="form-group ">
                                                                 <select class="form-control" name="product[{{$i}}][units]" id="units_{{$i}}">
-                                                                    <option value=0 id = 'unit_{{$i}}_0'>--Select--</option>
+                                                                    <option value='' id = 'unit_{{$i}}_0' selected="selected">--Select--</option>
                                                                     <option value=1 id = 'unit_{{$i}}_1'>KG</option>
                                                                     <option value=2 id = 'unit_{{$i}}_2'>Pieces</option>
                                                                     <option value=3 id = 'unit_{{$i}}_3'>Meter</option>
