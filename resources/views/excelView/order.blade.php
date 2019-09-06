79e25401f5baf0e14b1318617815d6aaaa2c2f42
Rakshiya <rakshiyas@agstech.co.in>
2019-09-06 17:15:20 +0530

    dd code reverted

--- resources/views/excelView/order.blade.php
+++ resources/views/excelView/order.blade.php
@@ -81,8 +81,8 @@
         </tr>
         <?php $count = 0; ?>
         @foreach($order['all_order_products'] as $product)
-        <?php dd($product->order_type); ?>
-        @if($count!=0 && isset($product->order_type) && $product->order_type =='order')
+        
+        <?php if($count!=0 && isset($product->order_type) && $product->order_type =='order') {?>
         <tr>
             <td></td>
             <td></td>
@@ -103,7 +103,7 @@
             <td></td>
             <td></td>
         </tr>
-        @endif
+        <?php } ?>
         <?php $count++; ?>
         @endforeach
         <?php $counter++; ?>
