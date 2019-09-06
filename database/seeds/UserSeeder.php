fca4492af49f8da69c8ac933586d0940140e62fc
Saranya Anuraj <saranya@syoware.com>
2019-09-06 13:18:29 +0530

    update user table

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder {
                 'access_token' => 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..i-oNJXc3VFt-3oPWVcWgOQ.kQgABF0cnhe7ZC6B3BbEfYIco33gcwMofBYlx70-ppi09QK24i4OEeqltc6WvNDKf9__NInhOC13J2HF85Q3PXvGztkRjdoa-t8gV7FD2j2kbv24Z2Un2FXye1PkYY4brWBmypcyS5oi1lUP7c1DCZu1nr_NAPJ05mFWbvpdzxJj9-pJrmwHuFJbgXUWjNeUKhDB4E1PiihxYf5kLONlVZ_J0VvoNnNRKyb5r-RO6GGp5wj4vbS5EPEMmfCzCynE2NVFrb5TjMdX7nBTk2FMuYQZeA146gXoaJVqJI-weK16gsGOr7zntn-11vn7DRxttRsREjYVKr2Jk5ive3kSukioc2kVTodRj08KgOlzjh9e3mRkCzsGT8Dy8HJGrmp2wQquh03T-Ah8OihBhRWayGVx3Xkp8Wxe8Q5Bk6pMv12k9nKARbvM6NYNXF6JIFMb3fpjPiqD-kwHUfykqHDcR1S3QauGt0M-zBAkBTt331Bmh_e8jAtKCXOGjy_k4QjeV9OuL7VowQhKOozkA8xlfsJ-BE_H9xLsZWJFHBbn_HZxViBLBkgO9eg8fKPo4fZr5nD9o5y7R8qPRAcmcwEbvx0JcFB9vi-hHcunff3k30YYZsiqhl0B4yzOiS13HjUoaa9kJEjB5FoU7wbxor4w94czCtPjYVftKtb185q_wHikls2JyipmE0LCvtVEHsxCUbVC3jdOMmJf3TtIWc-ygZav5dFsJ8hBvSxndOsDhSY.2SgY2wzDgKRsJWhYaKAfRA',
                 'refresh_token' => 'AB11576409863t9pNqU3xQhqsozUrD3m4WefjSHrTpzGnUBZ1K'
             ]);*/
-            DB::table('unit')->insert(
+            /*DB::table('unit')->insert(
                 array(
                     array(
                         'id' => '4',
@@ -29,7 +29,24 @@ class UserSeeder extends Seeder {
                         'unit_name' => 'mm'
                     ),
                    
-        ));         
+        ));         */
+
+        DB::table('users')->insert(
+                array(
+                    //Super Admin
+                    array(
+                        'id' => '20',
+                        'first_name' => 'Saranya',
+                        'last_name' => 'Admin',
+                        'phone_number' => '1234567890',
+                        'mobile_number' => '9898989890',
+                        'email' => 'sadmin@admin.com',
+                        // 'password' => '$2y$10$vkxXDsOalnfS.ANuYhDIkunjjtZGzvVD497Tvl0/vCnBAMKxkcaYC',
+                        'password' => '$2y$10$wraJ4uNWGMrftZnjpQcnuu.ar8OQQGntL2Hgol/eFT4Vp6V34Oaay',
+                        'role_id' => '0'
+                    )
+                   
+        ));
        }
 
     
