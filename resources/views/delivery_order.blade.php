601474c440d5db6c45e558752b4e24e00fb7aa34
navnath <navnathk@agstech.co.in>
2019-09-09 20:05:15 +0530

    Go To Page issue resolve in Delivery order/challan

--- resources/views/delivery_order.blade.php
+++ resources/views/delivery_order.blade.php
@@ -454,6 +454,7 @@
                             @if($delivery_data->lastPage() > 1)
                             <span style="margin-top:0px; margin-right: 0; padding-right: 0;" class="small pull-right">
                                 <form class="form-inline" method="GET" action="{{url('delivery_order')}}" id="filter_search">
+                                    <input type="hidden" name="order_status" value="{{($qstring_sort_type_order!="")?$qstring_sort_type_order:""}}"/>
                                     <div class="form-group">
                                         <label for="exampleInputName2"><b>Go To</b></label>
                                         &nbsp;
