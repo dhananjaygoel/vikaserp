f8fdbc830b03f9201e674cd434caaacf0357e9fc
Rakshiya <rakshiyas@agstech.co.in>
2019-09-18 12:39:24 +0530

    Update: Database Seeder updated.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder {
     public function run() {
 //		Model::unguard();
          //$this->call('UserSeeder');
-      //  $this->call('UserRolesSeeder');
-      //  $this->call('ProductTypeSeeder');
+       $this->call('UserRolesSeeder');
+       $this->call('ProductTypeSeeder');
         $this->call('UnitsSeeder');
         //$this->call('QuickbookToken');
         //$this->call('ProductSubCategorySeeder');
