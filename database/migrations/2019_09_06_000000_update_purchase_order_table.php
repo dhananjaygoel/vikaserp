574526aebf93dc0153948cf8cac11cd2aaa6e846
Rakshiya <rakshiyas@agstech.co.in>
2019-09-24 15:28:42 +0530

    migration file for purchase product table

---
+++ database/migrations/2019_09_06_000000_update_purchase_order_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePurchaseOrderTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('all_purchase_products', function (Blueprint $table) {
+            $table->string( 'length', 50 )->comments('Length field for table')->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
