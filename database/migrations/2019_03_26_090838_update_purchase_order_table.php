f212243227408b25e33c4df34cf29076e8b682f5
navnath <navnathk@agstech.co.in>
2019-09-26 19:00:27 +0530

    update purchase order table

--- database/migrations/2019_03_26_090838_update_purchase_order_table.php
+++ database/migrations/2019_03_26_090838_update_purchase_order_table.php
@@ -13,9 +13,9 @@ class UpdatePurchaseOrderTable extends Migration {
 	public function up()
 	{
         Schema::table('purchase_order', function (Blueprint $table) {
-			$table->string( 'vat_status', 50 )->comments('A=include_vat,P=exclude_vat')->nullable();
-			$table->integer('is_editable')->default(0);
-                               $table->string( 'length', 50 )->comments('Length field for table')->nullable();
+          $table->string( 'vat_status', 50 )->comments('A=include_vat,P=exclude_vat')->nullable();
+          $table->integer('is_editable')->default(0);
+          $table->string( 'length', 50 )->comments('Length field for table')->nullable();
         });
 	}
 
