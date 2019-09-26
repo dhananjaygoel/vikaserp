52c5fa804550fbbfc9c326b97b2535143878f136
navnath <navnathk@agstech.co.in>
2019-09-26 14:56:20 +0530

    migration for lenght field for purchase order table

--- database/migrations/2019_03_26_090838_update_purchase_order_table.php
+++ database/migrations/2019_03_26_090838_update_purchase_order_table.php
@@ -15,6 +15,7 @@ class UpdatePurchaseOrderTable extends Migration {
         Schema::table('purchase_order', function (Blueprint $table) {
 			$table->string( 'vat_status', 50 )->comments('A=include_vat,P=exclude_vat')->nullable();
 			$table->integer('is_editable')->default(0);
+                               $table->string( 'length', 50 )->comments('Length field for table')->nullable();
         });
 	}
 
