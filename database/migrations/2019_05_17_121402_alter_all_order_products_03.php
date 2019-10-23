663ec26a194e31ff8254bbe0879d689a03bd215c
Rakshiya <rakshiyas@agstech.co.in>
2019-10-23 20:03:14 +0530

    fix:migrate

--- database/migrations/2019_05_17_121402_alter_all_order_products_03.php
+++ database/migrations/2019_05_17_121402_alter_all_order_products_03.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterAllOrderProducts03 extends Migration {
+class AlterAllOrderProducts033 extends Migration {
 
 	/**
 	 * Run the migrations.
