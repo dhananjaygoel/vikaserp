988e258599a1e24130f72c896545889361c12dd1
Rakshiya Siddiqui <rakshiyas@agstech.co.in>
2019-12-13 19:03:41 +0530

    update: data seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // $this->call(QuickbookTokenSeeder::class);
-        $this->call(UpdateDocNum::class);
+        $this->call('UpdateDocNum');
     }
 }
