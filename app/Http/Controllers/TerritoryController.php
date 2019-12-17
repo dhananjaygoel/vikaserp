6e27d9022c30ab800f9fb98f431a16920f404085
Rakshiya Siddiqui <rakshiyas@agstech.co.in>
2019-12-17 15:44:03 +0530

    fix: territory delete exception

--- app/Http/Controllers/TerritoryController.php
+++ app/Http/Controllers/TerritoryController.php
@@ -9,7 +9,7 @@ use App\Territory;
 use App\DeliveryLocation;
 use App\TerritoryLocation;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Input;
+use Input;
 use Hash;
 use Illuminate\Support\Facades\Auth;
 
