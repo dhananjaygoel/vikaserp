3df9ab1eb28448d701ac2e10bc90a207b024c001
Rakshiya Siddiqui <rakshiyas@agstech.co.in>
2019-12-18 18:20:18 +0530

    update set price

--- routes/web.php
+++ routes/web.php
@@ -265,7 +265,7 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('customers', 'CustomerController');
     Route::get('get_city', 'CustomerController@get_city');
     Route::any('set_price/{id}', 'CustomerController@set_price');
-    Route::resource('update_set_price', 'CustomerController@update_set_price');
+    Route::any('update_set_price', 'CustomerController@update_set_price');
     Route::get('bulk_set_price', 'CustomerController@bulk_set_price');
     Route::post('save_all_set_price', 'CustomerController@save_all_set_price');
     Route::resource('territory', 'TerritoryController');
