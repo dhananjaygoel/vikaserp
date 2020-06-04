58a0c8e180fd72ed97472529f6e3bdd981b5d3c5
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-06-04 16:46:50 +0530

    test

--- app/Http/Controllers/DeliveryOrderController.php
+++ app/Http/Controllers/DeliveryOrderController.php
@@ -1966,9 +1966,9 @@ class DeliveryOrderController extends Controller {
                     }
                     // whatsapp code starts here
                     if($send_whatsapp == "true"){
-                        $sid = $_ENV['TWILIO_SID'];
-                        $token = env('TWILIO_TOKEN');
-                        dd($sid);
+                        $sid = env('TWILIO_SID','');
+                        $token = env('TWILIO_TOKEN','');
+                        dd($token);
                         $twilio = new Client($sid, $token);
                         try{
                             $message = $twilio->messages
