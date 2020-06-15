e6db721ddc6200a9587c9496ec7cddaeba6c5cde
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-06-15 21:46:42 +0530

    PChallan

--- app/Http/Controllers/PurchaseChallanController.php
+++ app/Http/Controllers/PurchaseChallanController.php
@@ -193,7 +193,7 @@ class PurchaseChallanController extends Controller {
         $challan_id = DB::getPdo()->lastInsertId();
 
         $pr_c = PurchaseChallan::where('id','=',$challan_id)->with('purchase_order_single')->first();
-        $vat_status = $pr_c->purchase_order_single->vat_percentage;
+        $vat_status = (isset($pr_c->purchase_order_single->vat_percentage)?$pr_c->purchase_order_single->vat_percentage:0);
         if($vat_status == "" OR $vat_status == null){
             $date_letter = 'PC/' . $current_date . "/" . $challan_id.'A';
         }
