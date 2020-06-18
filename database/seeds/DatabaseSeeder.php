58c9f083924154d3f08660ae961f7cbe8c1f559c
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-06-18 11:32:33 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserRolesSeeder::class);
         $this->call(UnitsSeeder::class);
         $this->call(ProductTypeSeeder::class);
-        // $this->call(QuickbookTokenSeeder::class);
+        $this->call(QuickbookTokenSeeder::class);
         // $this->call(AddAdminUserSeeder::class);
         $this->call('UpdateDocNum');
         $this->call(UpdateCustomer::class);
