f57fe87f985d7fa0d54aac55f6385afc9e4b9fa2
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-06-22 12:57:22 +0530

    quickbook token

--- database/seeds/QuickbookTokenSeeder.php
+++ database/seeds/QuickbookTokenSeeder.php
@@ -20,18 +20,18 @@ class QuickbookTokenSeeder extends Seeder
                         'id' => '1',
                         'client' => 'ABfN2ld2b7J2FTN22uTqLPCFkx9f99Sj4YvDsMmVHD3mNUyuWU',
                         'secret' => '9peqxCWOzdpp1cEKPnAsPAAyEz77Z930q2QADnoe',
-                        'access_token' => 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..u-z3bOmxrr8Hln0hujzZNw.UKp_uMKiJIMppy2EM9EuaM31udJEw6SWLs4hnePAX_-bZ3sbJFqXjgjcrQB9_Y5A6EkFeMJF_DV7M7jYqmaUu6ja95hrEIZUQxtP9709AzU-6m9pN3rKkHcvh6V9Xw6mLwa06Gr5mJikxfmVplYWTmXQO1H9GrrVW1dmje94w9h5aLzNWRaoqK_4mtVLW4R1Pq5xnYxsnxActIqoBcKGzmN-VRzcLPTBTSELcIgiE1VzTOilipM6UaV721PCq8dnz2a6WH-Dktr55oxI4SyXvqHdTisSizQMIloaN050O7zwOChAZS3nLmiUlQSNeXAkQX-icBgm9KnLP-p3GhUmtT8T35o1MNEjpQcUBWpTvN9qZ30yu3tHS82uY1aqvlwRxTIsuH9jrae5kAttzK31qGDnEjCDjY4Ti-E-BdhhxGFx4iLH88ATYRZfxGcZHHBWtTBYzkpuIVujKcqqN7HJvLq9zHyWXmW98XkZaxmE7dw-TUoBjqQQqGfN3w8fAb0pvgRgWB8COnTOqpBJdlAJ2ysvpDvP7_sndG1p2QwOcmQFLa34ZNBUrccTslnx3ZgJcuhSDsO0ogXn7KQdJchE5SK3Q9QapYjyb35QtIcacjk5c6qHrk9gFjnknCBT7dJsXeyOfTKHrZwtSZuQ0Eal-cIQ2pPTrVOUfYOxZyKpO3j-wM5yRnX4yFWJrpQrUZXGgivqDG7TWrEsxHuw_-kw4i5OhWL3xIQ5UJeAheD9f_4.I3cTou4vfDUAxTcf2BEv6A',
-                        'refresh_token' => 'AB11601277621xsCRu0HuMt07LvdU8xQkxJSPK0zlfXwxEIF5t'
+                        'access_token' => 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..L0tcl2OFXlGObwq2JyTIzg.RO4vFe_YUtTInelGpvosNxrI8h9RLhCLJDFfin_Cpmg-F4KV1fzpKb_ohGbhL_bp04NATw35y4t1kxj9FyNGfd2C-61zkbpMAjP2t1VLJs0q6eQRdEX0R2VFb3Cas0OvZM8ZT38eSISPzyugWlT2IgkfO4RNv-W9fMVuwRqAYvThg7TBxLssKu6IelIGdtUC-wRZbonyZmnHcArRXcua5OutLMUmDOHbbZW5D-zXQ_3nghDldOjFVxI7YGhNgjdRoz0DlWuae7VpQdXw6F-oC5Q6L_vOep6aF0h5dz1ufF8Kb_J1i8dH-YaQK9bfpPdqu3OaHtk5c7nkNdPdtMYQh4vifRQngwy9uRXdgMyPQqMjr3IkVqfD9cph7ixJ005Za2heseYOiwBbh8RnQ39zg2gyfJUZZolIiiLa88e15RpDXFNF_LPXoocJb67pQuccxIF-lQL6cbxzZUGXrAsxNWVlFsie5dZhBJWsvgOnZCY2HxeBeWOtMULh0YwbJxPpB6pDwaMaRLurLKQGtq498Y1jQ_K2S0UNMAfcmyRniuTHBgd7TXvTzk38U8jseIGMzlRbhE91wk_pf_HheorczVzB4tdg9MCLaTOXf2F_QfSQKoaZHdn7UTSehcs_d-ypp9bZl4plVRV8dEZ0h8iQks0ZO4NVGcBBgpbSRx-Smlf9HCmW_ZuDxJidn5pbbUkPC0U2fWKvSTVyUQwu_QdpGp61-5ChZvBwi5jZhVm_mGQ.NfJDwOv_yJeEXenC1G2d1A',
+                        'refresh_token' => 'AB11601537141XRI5NBJiWnODezChMqb64520VBuRsx90vNhsu'
                     ),
                     //Plus GST account
                     array(
                         'id' => '2',
                         'client' => 'ABSSab3mSZlIGJLQ1tCZSKKpd7LN7OI3zacIfFW0hLsHLVMnyp',
                         'secret' => 'PQLjc67R23HENkuvNmgj2YY3QVn1OcBHXNtIJvpP',
-                        'access_token' => 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..LvF9chYYml1MpqgDWYsujg.vf-yuOnlglCEyzqWTa_u0Xe3omA1RGSCuvpixgkNDnXeVADztmxwMB86Os1JlOGbFiT_hyHOwR93lSk2omO88Qs0KuCrWYjy0imjHKWu4uz2KVoSKxnW7fTlZzVSU71kuv3Z-2WQ_iuCMW_TNeQ8u85XH6ZWh9SH7gMM6kVrMHh87ugFfhu31gEJWNxNmQlkfV9-x6jGJ7VdHlFjksHYZS2jKKaayfJUO46HPN6DX9JFTRlsklCadRdccqdPwsUHB7TPVrvLvp1h8NrxbAxIoqLUByx-DRb4I8dmZFEYB6BoAfhQKyGskAJctV4Wq_lzIAvABzJ25NyPX15c6CnCneeFafXM8yuDeR_j0WLirREAZIgMIsdKuDnXH5DMaO1ToXW7JtIo6G3_VVaLB6xcsejm3UNqj3H6IY-z6PLecqXvyocVnDKpVn8_l0iRiuC5pNQixov1p-fqy5-storC3SYLYxDubDOzPBUs7Mw0DMagMwpJ5lWio_ARbp4L2dON7jCwR4U4Hu5P6JISselhsQ3BGqLscf_CsxvlqRU317OBVTgsR2YgZm0s7HXTG4T0Lin-zYSdHa4P-Nf_6yyrqMslLK3djCfMxzRi8AYU71b_0uW8vMQ3Apo7LHLIG8TQGQaVuSFEPEFtwM7QJrAg4I7DGaINsJH2Sm_R5nZgijF5UpixDKafJNrf-kLv60DaPW0AsjedUR6HAm1Y4eKN8UCt8GITFyIQXhgbrZDW1ac.2YcBLEmwt92HC1sFdFNSpQ',
-                        'refresh_token' => 'AB11601277570V5T7r3loCI1p08aFK5DXZjPKALGrJlHPo0JQ3'
+                        'access_token' => 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..3rnE-NVVWtYOOhQ81LS2OA.TgKyVNgHA2KLWhcpZjAFnG90NCwANdLZU4G5B2atczV_BLDyoHHmHGTF23nz4sHWlSXRkd6Bcwkkur4IRmMGKZI8iVRpvNT75LWdedKmX7IuzeAzCYs0yLTmOE1Vj988PYCH-3ZO1meefQH4CXZh6KCB3i3JRutxG2H3OM3PLVlJnO9TFUO_8o07pe9GlUZWLpl0Q7FCatsq-C9eCzc4CEIcAPPEmQOj7-r8GI5-uR22Wyc1h9TqJrNaWViSLbVRLWBhTJU7ixKu27Zbd2octgpcEonaoJlcxKBfr5aSaaegSbMa9rt6ojaDuVH3EZT80JBv0hB4_Z75Nh-h3PSnLlFRFn_2ctGc6tenYfOjav1obJVNE8idrLCdLY38oWN3GpCj1BqjFZE84-ZHjS4_mqFIAV7Ij6CV2-U9I1PrmW4SIznSJEn7k99eS3X1M71HLT1QHAfo5mIMgo5ZqlyEk84UKKgID5IMpeJ_qxig9TT-WE9A8VeubU6BIv4sm4XGi-upvgqGwPUKbAbhst0f0ptRiTjBISujxCpdHx0CWH4NXDv36_pQBKT_1k9r-kcCD_x4JBghX2lfkSDb4It--rJfxWvBKY_tlTWacMOsN4yuHezVqyPrEzQPoB4A2J7tCKMZ3mZVO2HF5_rJJLMi-Sx4TjJ5BamEJV2jTXIViRU_XmTvWlKD4kfEYfAvFnsKjmuQnuQ3EMB9JxZ7Tlrd4sXFKikT6_rhI2b3JRi7p7o.asHP35USnykplkj3Ersdag',
+                        'refresh_token' => 'AB11601537033OHJJLIMDbUCefklQ880Ioi4OR9ZMA3ThBQEh6'
                     )
         ));
-        dd('success');
+        echo 'success';
     }
 }
