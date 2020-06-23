d3b2aaadea43e1fbb1ae092b9655f116c679bce6
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-06-23 14:46:47 +0530

    database connection on URL

--- config/database.php
+++ config/database.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'staging.vikaserp.in' ? env('DB_CONNECTION', 'mysql1') : env('DB_CONNECTION', 'mysql')),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,6 +53,26 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'mysql1' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_STAGING_HOST', '127.0.0.1'),
+            'port' => env('DB_STAGING_PORT', '3306'),
+            'database' => env('DB_STAGING_DATABASE', 'forge'),
+            'username' => env('DB_STAGING_USERNAME', 'forge'),
+            'password' => env('DB_STAGING_PASSWORD', ''),
+            'unix_socket' => env('DB_STAGING_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
