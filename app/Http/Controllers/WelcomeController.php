25e4aafd351a32e4b1f17bbca849e2b7c18f4a4f
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-07-17 21:47:36 +0530

    send pdf

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2088,17 +2088,11 @@ class WelcomeController extends Controller {
             $allowed = false;
         }
         if ($allowed) {
-            // DB::table('file_info')->where('uuid',$uuid)->update(array('status'=> 1));
+            DB::table('file_info')->where('uuid',$uuid)->update(array('status'=> 1));
             $file_name = $file_data->file_name;
-
             $headers = [
                 'Content-type' => 'application/pdf'];
             return Storage::download(getcwd().$file_path, $file_name,$headers);
-           
-            // Storage::get(getcwd().$file_path);
-            // $response = Response::make($content, 200);
-            // chmod(getcwd().$file_path,0777);
-            // return Storage::download(getcwd().$file_path);
             exit(); // downloadable file
         } else {
             return view('notfound');
