401f498d5c7e3e4da8ef6c3d546051dbae8eea44
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-07-21 17:36:11 +0530

    js

--- public/assets/custom_js/custom_script.js
+++ public/assets/custom_js/custom_script.js
@@ -910,7 +910,7 @@ $('#discount_amount').on('blur', function () {
                 success: function (data) {
                     var main_array = JSON.parse(data);
                     var arr1 = main_array['data_array'];
-                    $('#product_price_' + rowId).val(arr1[0]['product_price']);
+                    $('#product_price_' + rowId).val(arr1[0]['product_price'].toFixed(0));
                 },
             });
         }
