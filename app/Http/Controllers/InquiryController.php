04335ae1aa771e67ebfde0dc5f017a8a4b592205
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-08-14 20:05:47 +0530

    sales staff

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -366,33 +366,11 @@ class InquiryController extends Controller {
         $ec = new WelcomeController();
         $ec->set_updated_date_to_sync_table($tables);
         /* end code */
-
-        $ip = Security::all();
-        $ip_array = [];
-        $ipaddress = '';
-        if (isset($ip) && !$ip->isEmpty()) {
-            foreach ($ip as $key => $value) {
-                $ip_array[$key] = $value->ip_address;
-            }
-            if (getenv('HTTP_CLIENT_IP'))
-                $ipaddress = getenv('HTTP_CLIENT_IP');
-            else if (getenv('HTTP_X_FORWARDED_FOR'))
-                $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-            else if (getenv('HTTP_X_FORWARDED'))
-                $ipaddress = getenv('HTTP_X_FORWARDED');
-            else if (getenv('HTTP_FORWARDED_FOR'))
-                $ipaddress = getenv('HTTP_FORWARDED_FOR');
-            else if (getenv('HTTP_FORWARDED'))
-                $ipaddress = getenv('HTTP_FORWARDED');
-            else if (getenv('REMOTE_ADDR'))
-                $ipaddress = getenv('REMOTE_ADDR');
-            else
-                $ipaddress = 'UNKNOWN';
-        }    
-        if(!in_array($ipaddress, $ip_array) && Auth::user()->role_id == 2){
+        
+        if(Auth::user()->role_id == 2){
             return redirect('dashboard')->with('flash_success_message', 'Inquiry details successfully added'.$whatsapp_error);
         }
-
+  
         return redirect('inquiry')->with('flash_success_message', 'Inquiry details successfully added'.$whatsapp_error);
     }
 
