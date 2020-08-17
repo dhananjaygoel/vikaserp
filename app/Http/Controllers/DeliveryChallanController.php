77f050f0391f30032f05b18bcf843fc9b61f4e38
rakshiyas <rakshiya.siddiqui@alphalogiclimited.com>
2020-08-17 11:57:26 +0530

    invoice

--- app/Http/Controllers/DeliveryChallanController.php
+++ app/Http/Controllers/DeliveryChallanController.php
@@ -1185,7 +1185,8 @@ class DeliveryChallanController extends Controller {
         if(Auth::user()->role_id != 0){
             if($update_delivery_challan->is_print_user != 0){
                 \Illuminate\Support\Facades\Session::flash('flash_message_err', 'You can not print many time, please contact your administrator');
-                return redirect('delivery_challan?status_filter=completed');
+                // return redirect('delivery_challan?status_filter=completed');
+                return Redirect::back()->withInput()->with('error', 'You can not print many time, please contact your administrator');
             }
         }
         if($update_delivery_challan->doc_number){
